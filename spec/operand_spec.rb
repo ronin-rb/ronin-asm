09c852f63844b34038010e73144a1724dc405d36
Postmodern <postmodern.mod3@gmail.com>
2025-02-21 01:39:50 -0800

    Added `Operand#type`.

--- spec/operand_spec.rb
+++ spec/operand_spec.rb
@@ -2,4 +2,16 @@ require 'spec_helper'
 require 'ronin/asm/operand'
 
 describe Ronin::ASM::Operand do
+  class TestOperand < Ronin::ASM::Operand
+  end
+
+  describe "#type" do
+    subject { TestOperand.new }
+
+    it do
+      expect {
+        subject.type
+      }.to raise_error(NotImplementedError,"#{subject.class}#type was not defined")
+    end
+  end
 end
