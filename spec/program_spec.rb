9756cc0bce56be6d64645bd2e65d1aff57727daa
Postmodern <postmodern.mod3@gmail.com>
2025-02-21 00:58:35 -0800

    Changed `Program#allocated_registers` to be a `Set`.

--- spec/program_spec.rb
+++ spec/program_spec.rb
@@ -8,9 +8,19 @@ describe Ronin::ASM::Program do
     end
   end
 
+  describe "#allocated_registers" do
+    it "must return a Set" do
+      expect(subject.allocated_registers).to be_kind_of(Set)
+    end
+  end
+
   describe "#initialize" do
     it "must default the architecture to :x86_64" do
       expect(subject.arch).to eq(:x86_64)
+    end
+
+    it "must initialize #allocated_registers to an empty Set" do
+      expect(subject.allocated_registers).to eq(Set.new)
     end
 
     context "when the arch: keyword argument is :x86" do
