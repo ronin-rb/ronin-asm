36ca3b6efd2977813c1dc1a9c521011bc7fa3a08
Postmodern <postmodern.mod3@gmail.com>
2025-02-21 11:23:40 -0800

    Test the size of the immediate value from smallest to biggest.

--- spec/immediate_spec.rb
+++ spec/immediate_spec.rb
@@ -18,28 +18,16 @@ describe Ronin::ASM::Immediate do
 
   describe "#width" do
     context "when #width is not explicitly set by #initialize" do
-      context "and when between 0x100000000 .. 0xffffffffffffffff" do
-        subject { described_class.new(0xffffffffffffffff).width }
+      context "and when between 0x00 .. 0xff" do
+        subject { described_class.new(0xff).width }
 
-        it { expect(subject).to be == 8 }
+        it { expect(subject).to be == 1 }
       end
 
-      context "and when between -0x800000000 .. -0x7fffffffffffffff" do
-        subject { described_class.new(-0x7fffffffffffffff).width }
+      context "and when between 0x00 .. -0x7f" do
+        subject { described_class.new(-0x7f).width }
 
-        it { expect(subject).to be == 8 }
-      end
-
-      context "and when between 0x10000 .. 0xffffffff" do
-        subject { described_class.new(0xffffffff).width }
-
-        it { expect(subject).to be == 4 }
-      end
-
-      context "and when between -0x80000 .. -0x7fffffff" do
-        subject { described_class.new(-0x7fffffff).width }
-
-        it { expect(subject).to be == 4 }
+        it { expect(subject).to be == 1 }
       end
 
       context "and when between 0x100 .. 0xffff" do
@@ -54,16 +42,28 @@ describe Ronin::ASM::Immediate do
         it { expect(subject).to be == 2 }
       end
 
-      context "and when between 0x00 .. 0xff" do
-        subject { described_class.new(0xff).width }
+      context "and when between 0x10000 .. 0xffffffff" do
+        subject { described_class.new(0xffffffff).width }
 
-        it { expect(subject).to be == 1 }
+        it { expect(subject).to be == 4 }
       end
 
-      context "and when between 0x00 .. -0x7f" do
-        subject { described_class.new(-0x7f).width }
+      context "and when between -0x80000 .. -0x7fffffff" do
+        subject { described_class.new(-0x7fffffff).width }
 
-        it { expect(subject).to be == 1 }
+        it { expect(subject).to be == 4 }
+      end
+
+      context "and when between 0x100000000 .. 0xffffffffffffffff" do
+        subject { described_class.new(0xffffffffffffffff).width }
+
+        it { expect(subject).to be == 8 }
+      end
+
+      context "and when between -0x800000000 .. -0x7fffffffffffffff" do
+        subject { described_class.new(-0x7fffffffffffffff).width }
+
+        it { expect(subject).to be == 8 }
       end
     end
   end
