b38e0dab512f9cc288bc40ef2fd746087c088edf
Postmodern <postmodern.mod3@gmail.com>
2025-02-21 23:53:01 -0800

    Add programmatically generated x86 instruction classes (closes #7).

---
+++ lib/ronin/asm/x86/instructions/vfnmadd213ps.rb
@@ -0,0 +1,142 @@
+# frozen_string_literal: true
+#
+# ronin-asm - A Ruby DSL for crafting Assembly programs and shellcode.
+#
+# Copyright (c) 2007-2025 Hal Brodigan (postmodern.mod3 at gmail.com)
+#
+# ronin-asm is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# ronin-asm is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with ronin-asm.  If not, see <https://www.gnu.org/licenses/>.
+#
+
+#
+# NOTE: this file was automatically generated. Do not edit!
+#
+
+require_relative '../instruction'
+
+module Ronin
+  module ASM
+    module X86
+      module Instructions
+        #
+        # Fused Negative Multiply-Add of Packed Single-Precision Floating-Point Values
+        #
+        class VFNMADD213PS < Instruction
+
+          #
+          # Initializes the `vfnmadd213ps` instruction.
+          #
+          # @param [Array<Operand>] operands
+          #   The operands for the instruction.
+          #
+          # @param [Hash{Symbol => Object}] kwargs
+          #   Additional keyword arguments.
+          #
+          # @option kwargs [String, nil] :comment
+          #   Optional comment for the instruction.
+          #
+          def initialize(*operands,**kwargs)
+            super(:vfnmadd213ps,*operands,**kwargs)
+          end
+
+          #
+          # Encodes the `vfnmadd213ps` instruction.
+          #
+          # @param [Encoder] encoder
+          # @return [Integer]
+          #
+          # @api private
+          #
+          def encode(encoder)
+            if @operands.length == 3 && @operands[0].type == :"xmm{k}{z}" && @operands[1].type == :xmm && @operands[2].type == :"mem128/m32bcst"
+              encoder.write_evex(mmm: 0b010, pp: 0b01, w: 0, ll: 0b00, vvvv: @operands[1], v: 0, rr: 0b00, _B: 0, x: 0, b: @operands[2], aaa: @operands[0], z: @operands[0], disp8xN: 16) +
+              encoder.write_opcode(0xac) +
+              encoder.write_modrm(@operands[2],@operands[0],@operands[2])
+            elsif @operands.length == 3 && @operands[0].type == :"xmm{k}{z}" && @operands[1].type == :xmm && @operands[2].type == :xmm
+              encoder.write_evex(mmm: 0b010, pp: 0b01, w: 0, ll: 0b00, vvvv: @operands[1], v: 0, rr: 0b00, _B: 0, x: 0, b: 0, aaa: @operands[0], z: @operands[0]) +
+              encoder.write_opcode(0xac) +
+              encoder.write_modrm(0b11,@operands[0],@operands[2])
+            elsif @operands.length == 3 && @operands[0].type == :"ymm{k}{z}" && @operands[1].type == :ymm && @operands[2].type == :"mem256/m32bcst"
+              encoder.write_evex(mmm: 0b010, pp: 0b01, w: 0, ll: 0b01, vvvv: @operands[1], v: 0, rr: 0b00, _B: 0, x: 0, b: @operands[2], aaa: @operands[0], z: @operands[0], disp8xN: 32) +
+              encoder.write_opcode(0xac) +
+              encoder.write_modrm(@operands[2],@operands[0],@operands[2])
+            elsif @operands.length == 3 && @operands[0].type == :"ymm{k}{z}" && @operands[1].type == :ymm && @operands[2].type == :ymm
+              encoder.write_evex(mmm: 0b010, pp: 0b01, w: 0, ll: 0b01, vvvv: @operands[1], v: 0, rr: 0b00, _B: 0, x: 0, b: 0, aaa: @operands[0], z: @operands[0]) +
+              encoder.write_opcode(0xac) +
+              encoder.write_modrm(0b11,@operands[0],@operands[2])
+            elsif @operands.length == 3 && @operands[0].type == :"zmm{k}{z}" && @operands[1].type == :zmm && @operands[2].type == :"mem512/m32bcst"
+              encoder.write_evex(mmm: 0b010, pp: 0b01, w: 0, ll: 0b10, vvvv: @operands[1], v: 0, rr: 0b00, _B: 0, x: 0, b: @operands[2], aaa: @operands[0], z: @operands[0], disp8xN: 64) +
+              encoder.write_opcode(0xac) +
+              encoder.write_modrm(@operands[2],@operands[0],@operands[2])
+            elsif @operands.length == 3 && @operands[0].type == :"zmm{k}{z}" && @operands[1].type == :zmm && @operands[2].type == :zmm
+              encoder.write_evex(mmm: 0b010, pp: 0b01, w: 0, vvvv: @operands[1], v: 0, rr: 0b00, _B: 0, x: 0, b: 0, aaa: @operands[0], z: @operands[0]) +
+              encoder.write_opcode(0xac) +
+              encoder.write_modrm(0b11,@operands[0],@operands[2])
+            elsif @operands.length == 3 && @operands[0].type == :xmm && @operands[1].type == :xmm && @operands[2].type == :"mem128/m32bcst"
+              encoder.write_evex(mmm: 0b010, pp: 0b01, w: 0, ll: 0b00, vvvv: @operands[1], v: 0, rr: 0b00, _B: 0, x: 0, b: @operands[2], aaa: 0, z: 0, disp8xN: 16) +
+              encoder.write_opcode(0xac) +
+              encoder.write_modrm(@operands[2],@operands[0],@operands[2])
+            elsif @operands.length == 3 && @operands[0].type == :xmm && @operands[1].type == :xmm && @operands[2].type == :xmm
+              encoder.write_vex(type: :vex, w: 0, l: 0, m_mmmm: 0b00010, pp: 0b01, r: 0, x: 0, b: 0, vvvv: @operands[1]) +
+              encoder.write_opcode(0xac) +
+              encoder.write_modrm(0b11,@operands[0],@operands[2])
+            elsif @operands.length == 3 && @operands[0].type == :xmm && @operands[1].type == :xmm && @operands[2].type == :xmm
+              encoder.write_evex(mmm: 0b010, pp: 0b01, w: 0, ll: 0b00, vvvv: @operands[1], v: 0, rr: 0b00, _B: 0, x: 0, b: 0, aaa: 0, z: 0) +
+              encoder.write_opcode(0xac) +
+              encoder.write_modrm(0b11,@operands[0],@operands[2])
+            elsif @operands.length == 3 && @operands[0].type == :xmm && @operands[1].type == :xmm && @operands[2].type == :m128
+              encoder.write_vex(type: :vex, w: 0, l: 0, m_mmmm: 0b00010, pp: 0b01, r: 0, x: 0, b: 0, vvvv: @operands[1]) +
+              encoder.write_opcode(0xac) +
+              encoder.write_modrm(@operands[2],@operands[0],@operands[2])
+            elsif @operands.length == 3 && @operands[0].type == :ymm && @operands[1].type == :ymm && @operands[2].type == :"mem256/m32bcst"
+              encoder.write_evex(mmm: 0b010, pp: 0b01, w: 0, ll: 0b01, vvvv: @operands[1], v: 0, rr: 0b00, _B: 0, x: 0, b: @operands[2], aaa: 0, z: 0, disp8xN: 32) +
+              encoder.write_opcode(0xac) +
+              encoder.write_modrm(@operands[2],@operands[0],@operands[2])
+            elsif @operands.length == 3 && @operands[0].type == :ymm && @operands[1].type == :ymm && @operands[2].type == :ymm
+              encoder.write_vex(type: :vex, w: 0, l: 1, m_mmmm: 0b00010, pp: 0b01, r: 0, x: 0, b: 0, vvvv: @operands[1]) +
+              encoder.write_opcode(0xac) +
+              encoder.write_modrm(0b11,@operands[0],@operands[2])
+            elsif @operands.length == 3 && @operands[0].type == :ymm && @operands[1].type == :ymm && @operands[2].type == :ymm
+              encoder.write_evex(mmm: 0b010, pp: 0b01, w: 0, ll: 0b01, vvvv: @operands[1], v: 0, rr: 0b00, _B: 0, x: 0, b: 0, aaa: 0, z: 0) +
+              encoder.write_opcode(0xac) +
+              encoder.write_modrm(0b11,@operands[0],@operands[2])
+            elsif @operands.length == 3 && @operands[0].type == :ymm && @operands[1].type == :ymm && @operands[2].type == :m256
+              encoder.write_vex(type: :vex, w: 0, l: 1, m_mmmm: 0b00010, pp: 0b01, r: 0, x: 0, b: 0, vvvv: @operands[1]) +
+              encoder.write_opcode(0xac) +
+              encoder.write_modrm(@operands[2],@operands[0],@operands[2])
+            elsif @operands.length == 3 && @operands[0].type == :zmm && @operands[1].type == :zmm && @operands[2].type == :"mem512/m32bcst"
+              encoder.write_evex(mmm: 0b010, pp: 0b01, w: 0, ll: 0b10, vvvv: @operands[1], v: 0, rr: 0b00, _B: 0, x: 0, b: @operands[2], aaa: 0, z: 0, disp8xN: 64) +
+              encoder.write_opcode(0xac) +
+              encoder.write_modrm(@operands[2],@operands[0],@operands[2])
+            elsif @operands.length == 3 && @operands[0].type == :zmm && @operands[1].type == :zmm && @operands[2].type == :zmm
+              encoder.write_evex(mmm: 0b010, pp: 0b01, w: 0, vvvv: @operands[1], v: 0, rr: 0b00, _B: 0, x: 0, b: 0, aaa: 0, z: 0) +
+              encoder.write_opcode(0xac) +
+              encoder.write_modrm(0b11,@operands[0],@operands[2])
+            elsif @operands.length == 4 && @operands[0].type == :"zmm{k}{z}" && @operands[1].type == :zmm && @operands[2].type == :zmm && @operands[3].type == :"{er}"
+              encoder.write_evex(mmm: 0b010, pp: 0b01, w: 0, ll: @operands[3], vvvv: @operands[1], v: 0, rr: 0b00, _B: 0, x: 0, b: 1, aaa: @operands[0], z: @operands[0]) +
+              encoder.write_opcode(0xac) +
+              encoder.write_modrm(0b11,@operands[0],@operands[2])
+            elsif @operands.length == 4 && @operands[0].type == :zmm && @operands[1].type == :zmm && @operands[2].type == :zmm && @operands[3].type == :"{er}"
+              encoder.write_evex(mmm: 0b010, pp: 0b01, w: 0, ll: @operands[3], vvvv: @operands[1], v: 0, rr: 0b00, _B: 0, x: 0, b: 1, aaa: 0, z: 0) +
+              encoder.write_opcode(0xac) +
+              encoder.write_modrm(0b11,@operands[0],@operands[2])
+            else
+              raise(ArgumentError,"invalid operands given for instruction: #{@name} #{@operands.map(&:type).join(', ')}")
+            end
+          end
+
+        end
+      end
+    end
+  end
+end
