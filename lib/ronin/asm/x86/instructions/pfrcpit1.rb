01f936891d72d6a654babb16d7b64452ccb8813c
Postmodern <postmodern.mod3@gmail.com>
2025-02-21 23:53:01 -0800

    Add programmatically generated x86 instruction classes (closes #7).

---
+++ lib/ronin/asm/x86/instructions/pfrcpit1.rb
@@ -0,0 +1,80 @@
+# frozen_string_literal: true
+#
+# ronin-asm - A Ruby DSL for crafting Assembly programs and shellcode.
+#
+# Copyright (c) 2007-2025 Hal Brodigan (postmodern.mod3 at gmail.com)
+#
+# ronin-asm is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# ronin-asm is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with ronin-asm.  If not, see <https://www.gnu.org/licenses/>.
+#
+
+#
+# NOTE: this file was automatically generated. Do not edit!
+#
+
+require_relative '../instruction'
+
+module Ronin
+  module ASM
+    module X86
+      module Instructions
+        #
+        # Packed Floating-Point Reciprocal Iteration 1
+        #
+        class PFRCPIT1 < Instruction
+
+          #
+          # Initializes the `pfrcpit1` instruction.
+          #
+          # @param [Array<Operand>] operands
+          #   The operands for the instruction.
+          #
+          # @param [Hash{Symbol => Object}] kwargs
+          #   Additional keyword arguments.
+          #
+          # @option kwargs [String, nil] :comment
+          #   Optional comment for the instruction.
+          #
+          def initialize(*operands,**kwargs)
+            super(:pfrcpit1,*operands,**kwargs)
+          end
+
+          #
+          # Encodes the `pfrcpit1` instruction.
+          #
+          # @param [Encoder] encoder
+          # @return [Integer]
+          #
+          # @api private
+          #
+          def encode(encoder)
+            if @operands.length == 2 && @operands[0].type == :mm && @operands[1].type == :mm
+              encoder.write_opcode(0x0f) +
+              encoder.write_opcode(0x0f) +
+              encoder.write_modrm(0b11,@operands[0],@operands[1]) +
+              encoder.write_opcode(0xa6)
+            elsif @operands.length == 2 && @operands[0].type == :mm && @operands[1].type == :mem64
+              encoder.write_opcode(0x0f) +
+              encoder.write_opcode(0x0f) +
+              encoder.write_modrm(@operands[1],@operands[0],@operands[1]) +
+              encoder.write_opcode(0xa6)
+            else
+              raise(ArgumentError,"invalid operands given for instruction: #{@name} #{@operands.map(&:type).join(', ')}")
+            end
+          end
+
+        end
+      end
+    end
+  end
+end
---
+++ lib/ronin/asm/x86/instructions/vprotw.rb
@@ -0,0 +1,92 @@
+# frozen_string_literal: true
+#
+# ronin-asm - A Ruby DSL for crafting Assembly programs and shellcode.
+#
+# Copyright (c) 2007-2025 Hal Brodigan (postmodern.mod3 at gmail.com)
+#
+# ronin-asm is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# ronin-asm is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with ronin-asm.  If not, see <https://www.gnu.org/licenses/>.
+#
+
+#
+# NOTE: this file was automatically generated. Do not edit!
+#
+
+require_relative '../instruction'
+
+module Ronin
+  module ASM
+    module X86
+      module Instructions
+        #
+        # Packed Rotate Words
+        #
+        class VPROTW < Instruction
+
+          #
+          # Initializes the `vprotw` instruction.
+          #
+          # @param [Array<Operand>] operands
+          #   The operands for the instruction.
+          #
+          # @param [Hash{Symbol => Object}] kwargs
+          #   Additional keyword arguments.
+          #
+          # @option kwargs [String, nil] :comment
+          #   Optional comment for the instruction.
+          #
+          def initialize(*operands,**kwargs)
+            super(:vprotw,*operands,**kwargs)
+          end
+
+          #
+          # Encodes the `vprotw` instruction.
+          #
+          # @param [Encoder] encoder
+          # @return [Integer]
+          #
+          # @api private
+          #
+          def encode(encoder)
+            if @operands.length == 3 && @operands[0].type == :xmm && @operands[1].type == :xmm && @operands[2].type == :imm8
+              encoder.write_vex(type: :xop, w: 0, l: 0, m_mmmm: 0b01000, pp: 0b00, r: 0, x: 0, b: 0, vvvv: 0) +
+              encoder.write_opcode(0xc1) +
+              encoder.write_modrm(0b11,@operands[0],@operands[1]) +
+              encoder.write_immediate(@operands[2],1)
+            elsif @operands.length == 3 && @operands[0].type == :xmm && @operands[1].type == :xmm && @operands[2].type == :xmm
+              encoder.write_vex(type: :xop, w: 0, l: 0, m_mmmm: 0b01001, pp: 0b00, r: 0, x: 0, b: 0, vvvv: @operands[2]) +
+              encoder.write_opcode(0x91) +
+              encoder.write_modrm(0b11,@operands[0],@operands[1])
+            elsif @operands.length == 3 && @operands[0].type == :xmm && @operands[1].type == :xmm && @operands[2].type == :m128
+              encoder.write_vex(type: :xop, w: 1, l: 0, m_mmmm: 0b01001, pp: 0b00, r: 0, x: 0, b: 0, vvvv: @operands[1]) +
+              encoder.write_opcode(0x91) +
+              encoder.write_modrm(@operands[2],@operands[0],@operands[2])
+            elsif @operands.length == 3 && @operands[0].type == :xmm && @operands[1].type == :m128 && @operands[2].type == :imm8
+              encoder.write_vex(type: :xop, w: 0, l: 0, m_mmmm: 0b01000, pp: 0b00, r: 0, x: 0, b: 0, vvvv: 0) +
+              encoder.write_opcode(0xc1) +
+              encoder.write_modrm(@operands[1],@operands[0],@operands[1]) +
+              encoder.write_immediate(@operands[2],1)
+            elsif @operands.length == 3 && @operands[0].type == :xmm && @operands[1].type == :m128 && @operands[2].type == :xmm
+              encoder.write_vex(type: :xop, w: 0, l: 0, m_mmmm: 0b01001, pp: 0b00, r: 0, x: 0, b: 0, vvvv: @operands[2]) +
+              encoder.write_opcode(0x91) +
+              encoder.write_modrm(@operands[1],@operands[0],@operands[1])
+            else
+              raise(ArgumentError,"invalid operands given for instruction: #{@name} #{@operands.map(&:type).join(', ')}")
+            end
+          end
+
+        end
+      end
+    end
+  end
+end
