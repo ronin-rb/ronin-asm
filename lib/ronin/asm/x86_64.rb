fe0101866d8ffc929f56c60d1a185aab3e5e17d8
Postmodern <postmodern.mod3@gmail.com>
2025-02-23 21:35:13 -0800

    Add programmatically generated x86-64 instruction classes (closes #51).

--- lib/ronin/asm/x86_64.rb
+++ lib/ronin/asm/x86_64.rb
@@ -23,6 +23,7 @@ require_relative 'x86_64/registers'
 require_relative 'x86_64/memory'
 require_relative 'x86_64/memory_offset'
 require_relative 'x86_64/relative_offset'
+require_relative 'x86_64/instructions'
 require_relative 'x86_64/encoder'
 
 require 'set'
@@ -33,6 +34,8 @@ module Ronin
     # Contains x86-64 Architecture information.
     #
     module X86_64
+      include Instructions
+
       # Default word size
       WORD_SIZE = 8
 
---
+++ lib/ronin/asm/x86_64/instructions/cmpsd.rb
@@ -0,0 +1,84 @@
+# frozen_string_literal: true
+#
+# ronin-asm - A Ruby DSL for crafting Assembly programs and shellcode.
+#
+# Copyright (c) 2007-2025 Hal Brodigan (postmodern.mod3 at gmail.com)
+#
+# ronin-asm is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# ronin-asm is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with ronin-asm.  If not, see <https://www.gnu.org/licenses/>.
+#
+
+#
+# NOTE: this file was automatically generated. Do not edit!
+#
+
+require_relative '../instruction'
+
+module Ronin
+  module ASM
+    module X86_64
+      module Instructions
+        #
+        # Compare Scalar Double-Precision Floating-Point Values
+        #
+        class CMPSD < Instruction
+
+          #
+          # Initializes the `cmpsd` instruction.
+          #
+          # @param [Array<Operand>] operands
+          #   The operands for the instruction.
+          #
+          # @param [Hash{Symbol => Object}] kwargs
+          #   Additional keyword arguments.
+          #
+          # @option kwargs [String, nil] :comment
+          #   Optional comment for the instruction.
+          #
+          def initialize(*operands,**kwargs)
+            super(:cmpsd,*operands,**kwargs)
+          end
+
+          #
+          # Encodes the `cmpsd` instruction.
+          #
+          # @param [Encoder] encoder
+          # @return [Integer]
+          #
+          # @api private
+          #
+          def encode(encoder)
+            if @operands.length == 3 && @operands[0].type == :xmm && @operands[1].type == :xmm && @operands[2].type == :imm8
+              encoder.write_prefix(0xf2, mandatory: true) +
+              encoder.write_rex(mandatory: false, w: 0, r: @operands[0], b: @operands[1]) +
+              encoder.write_opcode(0x0f) +
+              encoder.write_opcode(0xc2) +
+              encoder.write_modrm(0b11,@operands[0],@operands[1]) +
+              encoder.write_immediate(@operands[2],1)
+            elsif @operands.length == 3 && @operands[0].type == :xmm && @operands[1].type == :mem64 && @operands[2].type == :imm8
+              encoder.write_prefix(0xf2, mandatory: true) +
+              encoder.write_rex(mandatory: false, w: 0, r: @operands[0], x: @operands[1], b: @operands[1]) +
+              encoder.write_opcode(0x0f) +
+              encoder.write_opcode(0xc2) +
+              encoder.write_modrm(@operands[1],@operands[0],@operands[1]) +
+              encoder.write_immediate(@operands[2],1)
+            else
+              raise(ArgumentError,"invalid operands given for instruction: #{@name} #{@operands.map(&:type).join(', ')}")
+            end
+          end
+
+        end
+      end
+    end
+  end
+end
