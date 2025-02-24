74cc873874ba46b5d5ed2bf6952c324351c30bea
Postmodern <postmodern.mod3@gmail.com>
2025-02-23 21:18:45 -0800

    Added code generation for the x86-64 architecture (issue #51).

---
+++ codegen/x86_64.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+#
+# ronin-asm - A Ruby DSL for crafting Assembly programs and shellcode.
+#
+# Copyright (c) 2007-2025 Hal Brodigan (postmodern.mod3 at gmail.com)
+#
+# ronin-asm is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# ronin-asm is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with ronin-asm.  If not, see <https://www.gnu.org/licenses/>.
+#
+
+require_relative 'x86_64/isa'
+require_relative 'x86_64/instructions_dir'
+require_relative 'x86_64/instructions_file'
+
+if $0 == __FILE__
+  instructions = CodeGen::X86_64::ISA.load
+
+  # Create the `lib/ronin/asm/x86_64/instructions/` directory and generate the
+  # individual instruction Ruby files within it.
+  CodeGen::X86_64::InstructionsDir.generate(instructions)
+
+  # Generate the `lib/ronin/asm/x86_64/instructions.rb` file.
+  CodeGen::X86_64::InstructionsFile.generate(instructions)
+end
