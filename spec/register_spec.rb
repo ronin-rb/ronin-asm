81e3575c1ddb0b43134d46c1e676b6f1d2fddde9
Postmodern <postmodern.mod3@gmail.com>
2025-02-25 15:50:47 -0800

    Added the `Register#reg16?` method (closes #75).

--- spec/register_spec.rb
+++ spec/register_spec.rb
@@ -95,6 +95,28 @@ describe Ronin::ASM::Register do
     end
   end
 
+  describe "#reg16?" do
+    context "when the #width is 2" do
+      let(:width) { 2 }
+
+      subject { described_class.new(name, width: width) }
+
+      it "must return true" do
+        expect(subject.reg16?).to be(true)
+      end
+    end
+
+    context "when the #type is not 2" do
+      let(:width) { 1 }
+
+      subject { described_class.new(name, width: width) }
+
+      it "must return false" do
+        expect(subject.reg16?).to be(false)
+      end
+    end
+  end
+
   describe "#+" do
     context "when given an Ronin::ASM::Memory" do
       let(:operand) do
