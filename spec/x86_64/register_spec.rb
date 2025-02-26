efb817e6a565b1d55a1fb66e3baa1deedc09f2c1
Postmodern <postmodern.mod3@gmail.com>
2025-02-26 01:12:08 -0800

    Changed `X86_64::Register` to inherit from `X86::Register`.

--- spec/x86_64/register_spec.rb
+++ spec/x86_64/register_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 require 'ronin/asm/x86_64/register'
 
 describe Ronin::ASM::X86_64::Register do
-  it "must inherit from Ronin::ASM::Register" do
-    expect(described_class).to be < Ronin::ASM::Register
+  it "must inherit from Ronin::ASM::X86::Register" do
+    expect(described_class).to be < Ronin::ASM::X86::Register
   end
 end
