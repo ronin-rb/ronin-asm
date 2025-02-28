d3496202313fe377c9dbdfb897afbe5afd45a340
Postmodern <postmodern.mod3@gmail.com>
2025-02-28 01:59:02 -0800

    Allow `X86::MemoryOffset#initialize` to accept an optional width.

--- spec/x86/memory_offset_spec.rb
+++ spec/x86/memory_offset_spec.rb
@@ -11,27 +11,40 @@ describe Ronin::ASM::X86::MemoryOffset do
       expect(subject.value).to eq(value)
     end
 
-    context "when the value has a bit length less or equal to 32bits" do
-      let(:value) { 0x11223344 }
+    context "when a width: keyword argument is given" do
+      let(:value) { 0xff }
+      let(:width) { 4 }
 
-      it "must set #width to 4" do
-        expect(subject.width).to eq(4)
-      end
+      subject { described_class.new(value, width: width) }
 
-      it "must set #type to :memoffs32" do
-        expect(subject.type).to eq(:memoffs32)
+      it "must set #width" do
+        expect(subject.width).to eq(width)
       end
     end
 
-    context "when the value has a bit length less or equal to 64bits" do
-      let(:value) { 0x1122334455667788 }
+    context "when no width: keyword argument is given" do
+      context "when the value has a bit length less or equal to 32bits" do
+        let(:value) { 0x11223344 }
 
-      it "must set #width to 8" do
-        expect(subject.width).to eq(8)
+        it "must set #width to 4" do
+          expect(subject.width).to eq(4)
+        end
+
+        it "must set #type to :memoffs32" do
+          expect(subject.type).to eq(:memoffs32)
+        end
       end
 
-      it "must set #type to :memoffs64" do
-        expect(subject.type).to eq(:memoffs64)
+      context "when the value has a bit length less or equal to 64bits" do
+        let(:value) { 0x1122334455667788 }
+
+        it "must set #width to 8" do
+          expect(subject.width).to eq(8)
+        end
+
+        it "must set #type to :memoffs64" do
+          expect(subject.type).to eq(:memoffs64)
+        end
       end
     end
   end
