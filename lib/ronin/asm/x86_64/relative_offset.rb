a691d905ea6ce908fff4e83e8c97d6fee4f6e635
Postmodern <postmodern.mod3@gmail.com>
2025-02-28 12:20:28 -0800

    Added `Ronin::ASM::X86_64::RelativeOffset` (closes #101).

---
+++ lib/ronin/asm/x86_64/relative_offset.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+#
+# ronin-asm - A Ruby DSL for crafting Assembly programs and shellcode.
+#
+# Copyright (c) 2007-2025 Hal Brodigan (postmodern.mod3 at gmail.com)
+#
+# ronin-asm is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# ronin-asm is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with ronin-asm.  If not, see <https://www.gnu.org/licenses/>.
+#
+
+require_relative '../x86/relative_offset'
+
+module Ronin
+  module ASM
+    module X86_64
+      #
+      # Represents a memory offset (`memoffs32` or `memoffs64`).
+      #
+      # @since 1.0.0
+      #
+      class RelativeOffset < X86::RelativeOffset
+      end
+    end
+  end
+end
