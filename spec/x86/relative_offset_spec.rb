37931f4e48ea1c7c559995ea42996423bcdb5b62
Postmodern <postmodern.mod3@gmail.com>
2025-02-28 01:54:38 -0800

    Allow `X86::RelativeOffset#initialize` to accept a width.

--- spec/x86/relative_offset_spec.rb
+++ spec/x86/relative_offset_spec.rb
@@ -11,27 +11,40 @@ describe Ronin::ASM::X86::RelativeOffset do
       expect(subject.value).to eq(value)
     end
 
-    context "when the value has a bit length less or equal to 8 bits" do
-      let(:value) { 0x10 }
+    context "when a width: keyword argument is given" do
+      let(:value) { 0xff }
+      let(:width) { 4 }
 
-      it "must set #width to 1" do
-        expect(subject.width).to eq(1)
-      end
+      subject { described_class.new(value, width: width) }
 
-      it "must set #type to :rel8" do
-        expect(subject.type).to eq(:rel8)
+      it "must set #width" do
+        expect(subject.width).to eq(width)
       end
     end
 
-    context "when the value has a bit length less or equal to 32bits" do
-      let(:value) { 0x11223344 }
+    context "when no width: keyword argument is given" do
+      context "and when the value has a bit length less or equal to 8 bits" do
+        let(:value) { 0x10 }
 
-      it "must set #width to 4" do
-        expect(subject.width).to eq(4)
+        it "must set #width to 1" do
+          expect(subject.width).to eq(1)
+        end
+
+        it "must set #type to :rel8" do
+          expect(subject.type).to eq(:rel8)
+        end
       end
 
-      it "must set #type to :rel32" do
-        expect(subject.type).to eq(:rel32)
+      context "and when the value has a bit length less or equal to 32bits" do
+        let(:value) { 0x11223344 }
+
+        it "must set #width to 4" do
+          expect(subject.width).to eq(4)
+        end
+
+        it "must set #type to :rel32" do
+          expect(subject.type).to eq(:rel32)
+        end
       end
     end
   end
