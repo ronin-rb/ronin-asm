53d7498f1bc2fabf170456a6571bd208f47917c0
Postmodern <postmodern.mod3@gmail.com>
2025-03-04 12:43:06 -0800

    Renamed all `emit_*` syntax methods to `format_*` (issue #130).

--- spec/syntax/common_spec.rb
+++ spec/syntax/common_spec.rb
@@ -8,78 +8,78 @@ require 'ronin/asm/label'
 describe Ronin::ASM::Syntax::Common do
   subject { described_class }
 
-  describe ".emit_keyword" do
+  describe ".format_keyword" do
     let(:name) { :_start }
 
     it "must convert a keyword to a String" do
-      expect(subject.emit_keyword(name)).to eq(name.to_s)
+      expect(subject.format_keyword(name)).to eq(name.to_s)
     end
   end
 
-  describe ".emit_register" do
+  describe ".format_register" do
     let(:register) { double('Register') }
 
     it do
       expect {
-        subject.emit_register(register)
-      }.to raise_error(NotImplementedError,"#{subject}.emit_register was not implemented")
+        subject.format_register(register)
+      }.to raise_error(NotImplementedError,"#{subject}.format_register was not implemented")
     end
   end
 
-  describe ".emit_integer" do
+  describe ".format_integer" do
     let(:int) { 255 }
 
     it "must convert it into a hexadecimal value" do
-      expect(subject.emit_integer(int)).to eq("0xff")
+      expect(subject.format_integer(int)).to eq("0xff")
     end
 
     context "when given a negative number" do
       let(:int) { -255 }
 
       it "must convert it into a hexadecimal value" do
-        expect(subject.emit_integer(int)).to eq("-0xff")
+        expect(subject.format_integer(int)).to eq("-0xff")
       end
     end
   end
 
-  describe ".emit_immediate" do
+  describe ".format_immediate" do
     let(:immediate) { double('Immediate') }
 
     it do
       expect {
-        subject.emit_immediate(immediate)
-      }.to raise_error(NotImplementedError,"#{subject}.emit_immediate was not implemented")
+        subject.format_immediate(immediate)
+      }.to raise_error(NotImplementedError,"#{subject}.format_immediate was not implemented")
     end
   end
 
-  describe ".emit_memory" do
+  describe ".format_memory" do
     let(:memory) { double('Memory') }
 
     it do
       expect {
-        subject.emit_memory(memory)
-      }.to raise_error(NotImplementedError,"#{subject}.emit_memory was not implemented")
+        subject.format_memory(memory)
+      }.to raise_error(NotImplementedError,"#{subject}.format_memory was not implemented")
     end
   end
 
-  describe ".emit_label" do
+  describe ".format_label" do
     let(:name)  { :_start  }
     let(:label) { Ronin::ASM::Label.new(name) }
 
     it "must append a ':' to the name" do
-      expect(subject.emit_label(label)).to eq('_start:')
+      expect(subject.format_label(label)).to eq('_start:')
     end
   end
 
-  describe ".emit_operand" do
+  describe ".format_operand" do
     context "when given an Immediate value" do
       let(:immediate) { Ronin::ASM::Immediate.new(0x41) }
       let(:string)    { double('formatted assembly') }
 
-      it "must call emit_immediate" do
-        expect(subject).to receive(:emit_immediate).with(immediate).and_return(string)
+      it "must call format_immediate" do
+        expect(subject).to receive(:format_immediate).with(immediate).and_return(string)
 
-        expect(subject.emit_operand(immediate)).to be(string)
+        expect(subject.format_operand(immediate)).to be(string)
       end
     end
 
@@ -89,10 +89,10 @@ describe Ronin::ASM::Syntax::Common do
       end
       let(:string) { double('formatted assembly') }
 
-      it "must call emit_immediate" do
-        expect(subject).to receive(:emit_register).with(register).and_return(string)
+      it "must call format_immediate" do
+        expect(subject).to receive(:format_register).with(register).and_return(string)
 
-        expect(subject.emit_operand(register)).to be(string)
+        expect(subject.format_operand(register)).to be(string)
       end
     end
 
@@ -103,10 +103,10 @@ describe Ronin::ASM::Syntax::Common do
       let(:memory) { Ronin::ASM::Memory.new(register) }
       let(:string) { double('formatted assembly') }
 
-      it "must call emit_immediate" do
-        expect(subject).to receive(:emit_memory).with(memory).and_return(string)
+      it "must call format_immediate" do
+        expect(subject).to receive(:format_memory).with(memory).and_return(string)
 
-        expect(subject.emit_operand(memory)).to be(string)
+        expect(subject.format_operand(memory)).to be(string)
       end
     end
 
@@ -114,60 +114,60 @@ describe Ronin::ASM::Syntax::Common do
       let(:symbol) { :_label }
       let(:string) { double('formatted assembly') }
 
-      it "must call emit_keyword" do
-        expect(subject).to receive(:emit_keyword).with(symbol).and_return(string)
+      it "must call format_keyword" do
+        expect(subject).to receive(:format_keyword).with(symbol).and_return(string)
 
-        expect(subject.emit_operand(symbol)).to be(string)
+        expect(subject.format_operand(symbol)).to be(string)
       end
     end
   end
 
-  describe ".emit_operands" do
+  describe ".format_operands" do
     let(:operands) do
       [double('operand1'), double('operand2'), double('operand3')]
     end
     let(:formatted_operands) { %w[op1 op2 op3] }
 
-    it "just call emit_operand on each operand and join them together with ',\\t'" do
-      expect(subject).to receive(:emit_operand).with(operands[0]).and_return(formatted_operands[0])
-      expect(subject).to receive(:emit_operand).with(operands[1]).and_return(formatted_operands[1])
-      expect(subject).to receive(:emit_operand).with(operands[2]).and_return(formatted_operands[2])
+    it "just call format_operand on each operand and join them together with ',\\t'" do
+      expect(subject).to receive(:format_operand).with(operands[0]).and_return(formatted_operands[0])
+      expect(subject).to receive(:format_operand).with(operands[1]).and_return(formatted_operands[1])
+      expect(subject).to receive(:format_operand).with(operands[2]).and_return(formatted_operands[2])
 
-      expect(subject.emit_operands(operands)).to eq(formatted_operands.join(",\t"))
+      expect(subject.format_operands(operands)).to eq(formatted_operands.join(",\t"))
     end
   end
 
-  describe ".emit_instruction" do
+  describe ".format_instruction" do
     let(:instruction) { double('Instruction') }
 
     it do
       expect {
-        subject.emit_instruction(instruction)
-      }.to raise_error(NotImplementedError,"#{subject}.emit_instruction was not implemented")
+        subject.format_instruction(instruction)
+      }.to raise_error(NotImplementedError,"#{subject}.format_instruction was not implemented")
     end
   end
 
-  describe ".emit_section" do
+  describe ".format_section" do
     let(:name) { :text }
 
     it do
       expect {
-        subject.emit_section(name)
-      }.to raise_error(NotImplementedError,"#{subject}.emit_section was not implemented")
+        subject.format_section(name)
+      }.to raise_error(NotImplementedError,"#{subject}.format_section was not implemented")
     end
   end
 
-  describe ".emit_prologue" do
+  describe ".format_prologue" do
     let(:program) { double('Program') }
 
     it do
       expect {
-        subject.emit_prologue(program)
-      }.to raise_error(NotImplementedError,"#{subject}.emit_prologue was not implemented")
+        subject.format_prologue(program)
+      }.to raise_error(NotImplementedError,"#{subject}.format_prologue was not implemented")
     end
   end
 
-  describe ".emit_program" do
+  describe ".format_program" do
     let(:immediate) { Ronin::ASM::Immediate.new(0x41) }
     let(:register) do
       Ronin::ASM::Register.new(:eax, number: 0, width: 4, type: :reg32)
@@ -194,26 +194,26 @@ describe Ronin::ASM::Syntax::Common do
       ]
     end
 
-    it "must call .emit_prologue, then .emit_section(:text), then .emit_label(:_start), and then .emit_instruction for each instruction" do
-      expect(subject).to receive(:emit_prologue).with(program).and_return(formatted_prologue)
-      expect(subject).to receive(:emit_section).with(:text).and_return(formatted_text_section)
+    it "must call .format_prologue, then .format_section(:text), then .format_label(:_start), and then .format_instruction for each instruction" do
+      expect(subject).to receive(:format_prologue).with(program).and_return(formatted_prologue)
+      expect(subject).to receive(:format_section).with(:text).and_return(formatted_text_section)
 
-      expect(subject).to receive(:emit_instruction).with(instructions[0]).and_return(formatted_instructions[0])
-      expect(subject).to receive(:emit_instruction).with(instructions[1]).and_return(formatted_instructions[1])
-      expect(subject).to receive(:emit_instruction).with(instructions[2]).and_return(formatted_instructions[2])
+      expect(subject).to receive(:format_instruction).with(instructions[0]).and_return(formatted_instructions[0])
+      expect(subject).to receive(:format_instruction).with(instructions[1]).and_return(formatted_instructions[1])
+      expect(subject).to receive(:format_instruction).with(instructions[2]).and_return(formatted_instructions[2])
 
-      subject.emit_program(program)
+      subject.format_program(program)
     end
 
     it "must tab indent each instruction line in the resulting string and append a newline" do
-      allow(subject).to receive(:emit_prologue).with(program).and_return(formatted_prologue)
-      allow(subject).to receive(:emit_section).with(:text).and_return(formatted_text_section)
+      allow(subject).to receive(:format_prologue).with(program).and_return(formatted_prologue)
+      allow(subject).to receive(:format_section).with(:text).and_return(formatted_text_section)
 
-      allow(subject).to receive(:emit_instruction).with(instructions[0]).and_return(formatted_instructions[0])
-      allow(subject).to receive(:emit_instruction).with(instructions[1]).and_return(formatted_instructions[1])
-      allow(subject).to receive(:emit_instruction).with(instructions[2]).and_return(formatted_instructions[2])
+      allow(subject).to receive(:format_instruction).with(instructions[0]).and_return(formatted_instructions[0])
+      allow(subject).to receive(:format_instruction).with(instructions[1]).and_return(formatted_instructions[1])
+      allow(subject).to receive(:format_instruction).with(instructions[2]).and_return(formatted_instructions[2])
 
-      expect(subject.emit_program(program)).to eq(
+      expect(subject.format_program(program)).to eq(
         [
           formatted_prologue,
           formatted_text_section,
@@ -247,29 +247,29 @@ describe Ronin::ASM::Syntax::Common do
         ]
       end
 
-      it "must call .emit_label with each Label" do
-        allow(subject).to receive(:emit_prologue).with(program).and_return(formatted_prologue)
-        allow(subject).to receive(:emit_section).with(:text).and_return(formatted_text_section)
-        allow(subject).to receive(:emit_label).with(:_start).and_return(formatted_start_label)
+      it "must call .format_label with each Label" do
+        allow(subject).to receive(:format_prologue).with(program).and_return(formatted_prologue)
+        allow(subject).to receive(:format_section).with(:text).and_return(formatted_text_section)
+        allow(subject).to receive(:format_label).with(:_start).and_return(formatted_start_label)
 
-        expect(subject).to receive(:emit_instruction).with(instructions[0]).and_return(formatted_instructions[0])
-        expect(subject).to receive(:emit_label).with(instructions[1]).and_return(formatted_instructions[1])
-        expect(subject).to receive(:emit_instruction).with(instructions[2]).and_return(formatted_instructions[2])
-        expect(subject).to receive(:emit_label).with(instructions[3]).and_return(formatted_instructions[3])
-        expect(subject).to receive(:emit_instruction).with(instructions[4]).and_return(formatted_instructions[4])
+        expect(subject).to receive(:format_instruction).with(instructions[0]).and_return(formatted_instructions[0])
+        expect(subject).to receive(:format_label).with(instructions[1]).and_return(formatted_instructions[1])
+        expect(subject).to receive(:format_instruction).with(instructions[2]).and_return(formatted_instructions[2])
+        expect(subject).to receive(:format_label).with(instructions[3]).and_return(formatted_instructions[3])
+        expect(subject).to receive(:format_instruction).with(instructions[4]).and_return(formatted_instructions[4])
 
-        subject.emit_program(program)
+        subject.format_program(program)
       end
 
       it "must not tab indent the labels in the resulting string" do
-        allow(subject).to receive(:emit_prologue).with(program).and_return(formatted_prologue)
-        allow(subject).to receive(:emit_section).with(:text).and_return(formatted_text_section)
+        allow(subject).to receive(:format_prologue).with(program).and_return(formatted_prologue)
+        allow(subject).to receive(:format_section).with(:text).and_return(formatted_text_section)
 
-        allow(subject).to receive(:emit_instruction).with(instructions[0]).and_return(formatted_instructions[0])
-        allow(subject).to receive(:emit_instruction).with(instructions[2]).and_return(formatted_instructions[2])
-        allow(subject).to receive(:emit_instruction).with(instructions[4]).and_return(formatted_instructions[4])
+        allow(subject).to receive(:format_instruction).with(instructions[0]).and_return(formatted_instructions[0])
+        allow(subject).to receive(:format_instruction).with(instructions[2]).and_return(formatted_instructions[2])
+        allow(subject).to receive(:format_instruction).with(instructions[4]).and_return(formatted_instructions[4])
 
-        expect(subject.emit_program(program)).to eq(
+        expect(subject.format_program(program)).to eq(
           [
             formatted_prologue,
             formatted_text_section,
