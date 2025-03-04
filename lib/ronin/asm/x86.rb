c2480d022a29dd457b25a8bca53e560132c87661
Postmodern <postmodern.mod3@gmail.com>
2025-03-03 17:09:37 -0800

    Removed `Program#instruction`.

--- lib/ronin/asm/x86.rb
+++ lib/ronin/asm/x86.rb
@@ -289,7 +289,7 @@ module Ronin
       # @param [Integer] number
       #   The interrupt number.
       #
-      def interrupt(number) = instruction(:int,number)
+      def interrupt(number) = int(number)
 
       #
       # Generates the instruction to invoke a syscall.
@@ -316,7 +316,7 @@ module Ronin
       # @param [Immediate, Memory, Register, Integer, Symbol] op
       #   The value.
       #
-      def stack_push(op) = instruction(:push,op)
+      def stack_push(op) = push(op)
 
       #
       # Generates the instruction to pop a value off of the Stack.
@@ -324,7 +324,7 @@ module Ronin
       # @param [Register] op
       #   The register operand to store the value.
       #
-      def stack_pop(op) = instruction(:pop,op)
+      def stack_pop(op) = pop(op)
 
       #
       # Generates the instruction to clear a register.
@@ -332,7 +332,7 @@ module Ronin
       # @param [Register] register
       #   The register to clear.
       #
-      def register_clear(register) = instruction(:xor,register,register)
+      def register_clear(register) = xor(register,register)
 
       #
       # Generates the instruction to set a register.
@@ -343,7 +343,7 @@ module Ronin
       # @param [Immediate, Memory, Register, Integer, Symbol] value
       #   The value to set.
       #
-      def register_set(register,value) = instruction(:mov,register,value)
+      def register_set(register,value) = mov(register,value)
 
       #
       # Generates the instruction to save a register.
