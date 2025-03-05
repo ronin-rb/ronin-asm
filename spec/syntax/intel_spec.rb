511bc0ebf98882e17a5164ec49b6ea41c8dd6cd9
Postmodern <postmodern.mod3@gmail.com>
2025-03-04 16:26:05 -0800

    Added `X86::Syntax::ATT` and `X86::Syntax::Intel` (issue #132).
    * Moved `Syntax::ATT.format_memory` into `X86::Syntax::ATT` since it
      relies on `Memory#index` and `Memory#scale` which are x86-isms (SIB).
    * Moved `Syntax::Intel.format_memory` into `X86::Syntax::Intel` since it
      relies on `Memory#index` and `Memory#scale` which are x86-isms (SIB).

--- spec/syntax/intel_spec.rb
+++ spec/syntax/intel_spec.rb
@@ -26,65 +26,6 @@ describe Ronin::ASM::Syntax::Intel do
     end
   end
 
-  describe ".format_memory" do
-    let(:register) { Ronin::ASM::X86::Register.new(:eax, width: 4) }
-    let(:operand)  { Ronin::ASM::X86::Memory.new(base: register) }
-
-    it "must enclose the memory in brackets" do
-      expect(subject.format_memory(operand)).to eq("[eax]")
-    end
-
-    context "when operand width does not match the base width" do
-      let(:width) { 2 }
-      let(:operand) do
-        Ronin::ASM::X86::Memory.new(base: register, width: width)
-      end
-
-      it "must specify the width" do
-        expect(subject.format_memory(operand)).to eq("WORD [eax]")
-      end
-    end
-
-    context "with an displacement" do
-      let(:displacement) { 255 }
-      let(:operand) do
-        Ronin::ASM::X86::Memory.new(base: register, displacement: displacement)
-      end
-
-      it "must add the displacement to the base" do
-        expect(subject.format_memory(operand)).to eq("[eax+0xff]")
-      end
-
-      context "when 0" do
-        let(:operand) { Ronin::ASM::X86::Memory.new(base: register, displacement: 0) }
-
-        it "must omit the displacement" do
-          expect(subject.format_memory(operand)).to eq("[eax]")
-        end
-      end
-    end
-
-    context "with an index" do
-      let(:index)   { Ronin::ASM::X86::Register.new(:esi, width: 4) }
-      let(:operand) { Ronin::ASM::X86::Memory.new(base: register, index: index) }
-
-      it "must add the index to the base" do
-        expect(subject.format_memory(operand)).to eq("[eax+esi]")
-      end
-
-      context "with a scale" do
-        let(:scale)   { 4 }
-        let(:operand) do
-          Ronin::ASM::X86::Memory.new(base: register, index: index, scale: scale)
-        end
-
-        it "must multiple the index by the scale" do
-          expect(subject.format_memory(operand)).to eq("[eax+esi*0x4]")
-        end
-      end
-    end
-  end
-
   describe ".format_instruction" do
     context "with no operands" do
       let(:instruction) { Ronin::ASM::Instruction.new(:ret) }
