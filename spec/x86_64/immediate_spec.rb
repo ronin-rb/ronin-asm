4dc384c19a011323af31e0a6c07ebdc7d2a7d5ed
Postmodern <postmodern.mod3@gmail.com>
2025-03-05 19:28:37 -0800

    Added the `X86_64::Immediate#to_s` method (closes #139).

--- spec/x86_64/immediate_spec.rb
+++ spec/x86_64/immediate_spec.rb
@@ -9,4 +9,16 @@ describe Ronin::ASM::X86_64::Immediate do
   it "must also include Ronin::ASM::X86_64::Operand" do
     expect(described_class).to include(Ronin::ASM::X86_64::Operand)
   end
+
+  let(:value) { 0xff }
+
+  subject { described_class.new(value) }
+
+  describe "#to_s" do
+    it "must call Ronin::ASM::X86_64::Syntax::Intel.format_immediate" do
+      expect(subject.to_s).to eq(
+        Ronin::ASM::X86_64::Syntax::Intel.format_immediate(subject)
+      )
+    end
+  end
 end
