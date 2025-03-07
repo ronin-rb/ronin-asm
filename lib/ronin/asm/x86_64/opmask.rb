d8c949a870517d5747d416553cea298e33381a42
Postmodern <postmodern.mod3@gmail.com>
2025-03-06 18:07:31 -0800

    Added the `X86_64::Opmask` class (closes #167).

---
+++ lib/ronin/asm/x86_64/opmask.rb
@@ -0,0 +1,48 @@
+# frozen_string_literal: true
+#
+# ronin-asm - A Ruby DSL for crafting Assembly programs and shellcode.
+#
+# Copyright (c) 2007-2025 Hal Brodigan (postmodern.mod3 at gmail.com)
+#
+# ronin-asm is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# ronin-asm is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with ronin-asm.  If not, see <https://www.gnu.org/licenses/>.
+#
+
+require_relative '../x86/opmask'
+require_relative 'decorator'
+
+module Ronin
+  module ASM
+    module X86_64
+      #
+      # Represents a operand mask decorator (ex: `{k1}` or `{k1}{z}`).
+      #
+      # @since 1.0.0
+      #
+      class Opmask < X86::Opmask
+
+        include Decorator
+
+        #
+        # Converts the opmask operand decorator into a String.
+        #
+        # @return [String]
+        #
+        def to_s = Syntax::Intel.format_opmask(self)
+
+      end
+    end
+  end
+end
+
+require_relative 'syntax/intel'
---
+++ spec/x86_64/opmask_spec.rb
@@ -0,0 +1,31 @@
+require 'spec_helper'
+require 'ronin/asm/x86_64/opmask'
+
+require 'ronin/asm/x86_64/register'
+
+describe Ronin::ASM::X86_64::Opmask do
+  it "must inherit from Ronin::ASM::X86::Opasm" do
+    expect(described_class).to be < Ronin::ASM::X86::Opmask
+  end
+
+  it "must include Ronin::ASM::X86_64::Decorator" do
+    expect(described_class).to include(Ronin::ASM::X86_64::Decorator)
+  end
+
+  let(:operand) do
+    Ronin::ASM::X86_64::Register.new(:xmm0, width: 16, number: 0, type: :xmm)
+  end
+  let(:k) do
+    Ronin::ASM::X86_64::Register.new(:k1, width: 8, number: 1, type: :k)
+  end
+
+  subject { described_class.new(operand,k) }
+
+  describe "#to_s" do
+    it "must return formatted operand and the opmask decorator" do
+      expect(subject.to_s).to eq(
+        Ronin::ASM::X86_64::Syntax::Intel.format_opmask(subject)
+      )
+    end
+  end
+end
