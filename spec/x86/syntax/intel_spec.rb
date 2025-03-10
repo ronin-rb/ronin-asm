68086a546faab094340b9a0f2aa85f41edb05537
Postmodern <postmodern.mod3@gmail.com>
2025-03-09 17:58:54 -0700

    Merged `Syntax::Intel` into `X86::Syntax::Intel` (closes #181).
    * Intel assembly syntax is unique to the x86 and x86-64 architectures.

--- spec/x86/syntax/intel_spec.rb
+++ spec/x86/syntax/intel_spec.rb
@@ -4,15 +4,33 @@ require 'ronin/asm/x86/syntax/intel'
 require 'ronin/asm/x86/immediate'
 require 'ronin/asm/x86/register'
 require 'ronin/asm/x86/memory'
+require 'ronin/asm/x86/instruction'
+require 'ronin/asm/program'
 
 require_relative 'common_examples'
 
 describe Ronin::ASM::X86::Syntax::Intel do
-  it "must inherit from Ronin::ASM::Syntax::Intel" do
-    expect(described_class).to be < Ronin::ASM::Syntax::Intel
+  it "must inherit from Ronin::ASM::X86::Syntax::Common" do
+    expect(described_class).to be < Ronin::ASM::X86::Syntax::Common
   end
 
   subject { described_class }
+
+  describe ".format_immediate" do
+    let(:operand) { Ronin::ASM::Immediate.new(255, width: 1) }
+
+    it "must prepend a size specifier" do
+      expect(subject.format_immediate(operand)).to eq("BYTE 0xff")
+    end
+  end
+
+  describe ".format_register" do
+    let(:register) { Ronin::ASM::Register.new(:eax, width: 4, type: :reg32) }
+
+    it "must return the register name" do
+      expect(subject.format_register(register)).to eq("eax")
+    end
+  end
 
   describe ".format_memory" do
     let(:register) { Ronin::ASM::X86::Register.new(:eax, width: 4) }
@@ -69,6 +87,107 @@ describe Ronin::ASM::X86::Syntax::Intel do
         it "must multiple the index by the scale" do
           expect(subject.format_memory(operand)).to eq("[eax+esi*0x4]")
         end
+      end
+    end
+  end
+
+  describe ".format_instruction" do
+    context "when the instruction has no operands" do
+      let(:instruction) { Ronin::ASM::Instruction.new(:ret) }
+
+      it "must return the instruction name only" do
+        expect(subject.format_instruction(instruction)).to eq(
+          instruction.name.to_s
+        )
+      end
+    end
+
+    context "when the instruction has operands" do
+      let(:operand1) { Ronin::ASM::Register.new(:eax, width: 4, type: :reg32) }
+      let(:operand2) { Ronin::ASM::Immediate.new(0xff, width: 1) }
+      let(:operands) { [operand1, operand2] }
+      let(:instruction) do
+        Ronin::ASM::Instruction.new(:mov, *operands)
+      end
+
+      it "must return the instruction name followed by the formatted operands, separated by tabs" do
+        expect(subject.format_instruction(instruction)).to eq(
+          "#{instruction.name}\t#{subject.format_operands(operands)}"
+        )
+      end
+    end
+  end
+
+  describe ".format_section" do
+    it "must return the section name" do
+      expect(subject.format_section(:text)).to eq("section .text")
+    end
+  end
+
+  describe ".format_program" do
+    let(:program) do
+      Ronin::ASM::Program.new(arch: :x86) do
+        mov eax, 0xff
+        ret
+      end
+    end
+
+    it "must output the _start label and the program" do
+      asm = subject.format_program(program)
+
+      expect(asm).to eq([
+        "BITS 32",
+        "section .text",
+        "_start:",
+        "\tmov\teax,\tBYTE 0xff",
+        "\tret",
+        ""
+      ].join($/))
+    end
+
+    context "when formatting labels" do
+      let(:program) do
+        Ronin::ASM::Program.new(arch: :x86) do
+          mov eax, 0
+
+          _loop do
+            inc eax
+            cmp eax, 10
+            jl  :_loop
+          end
+
+          ret
+        end
+      end
+
+      it "must format both labels and instructions" do
+        expect(subject.format_program(program)).to eq([
+          "BITS 32",
+          "section .text",
+          "_start:",
+          "\tmov\teax,\tBYTE 0x0",
+          "_loop:",
+          "\tinc\teax",
+          "\tcmp\teax,\tBYTE 0xa",
+          "\tjl\t_loop",
+          "\tret",
+          ""
+        ].join($/))
+      end
+    end
+
+    context "when the program arch is :amd64" do
+      let(:program) do
+        Ronin::ASM::Program.new(arch: :amd64) do
+          push rax
+          push rbx
+          mov  rax, 0xff
+          ret
+        end
+      end
+
+      it "must include start with the '.code64' directive" do
+        expect(subject.format_program(program)).to match(/^BITS 64$/)
       end
     end
   end
