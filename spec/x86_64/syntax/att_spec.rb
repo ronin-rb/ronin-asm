e65381695e7ea08506ad9b4c134df43aa1c940ff
Postmodern <postmodern.mod3@gmail.com>
2025-03-17 18:46:43 -0700

    Add different hardcoded `format_prologue` methods for each architecture.

--- spec/x86_64/syntax/att_spec.rb
+++ spec/x86_64/syntax/att_spec.rb
@@ -5,4 +5,34 @@ describe Ronin::ASM::X86_64::Syntax::ATT do
   it "must inherit from Ronin::ASM::X86::Syntax::ATT" do
     expect(described_class).to be < Ronin::ASM::X86::Syntax::ATT
   end
+
+  subject { described_class }
+
+  describe ".format_prologue" do
+    let(:program) do
+      Ronin::ASM::Program.new(arch: :x86_64) do
+        mov rax, 0xff
+        ret
+      end
+    end
+
+    it "must return '.code64'" do
+      expect(subject.format_prologue(program)).to eq('.code64')
+    end
+  end
+
+  describe ".format_program" do
+    let(:program) do
+      Ronin::ASM::Program.new(arch: :amd64) do
+        push rax
+        push rbx
+        mov 0xff, rax
+        ret
+      end
+    end
+
+    it "must include start with the '.code64' directive" do
+      expect(subject.format_program(program)).to match(/^\.code64$/)
+    end
+  end
 end
--- spec/x86_64/syntax/intel_spec.rb
+++ spec/x86_64/syntax/intel_spec.rb
@@ -5,4 +5,19 @@ describe Ronin::ASM::X86_64::Syntax::Intel do
   it "must inherit from Ronin::ASM::X86::Syntax::Intel" do
     expect(described_class).to be < Ronin::ASM::X86::Syntax::Intel
   end
+
+  subject { described_class }
+
+  describe ".format_prologue" do
+    let(:program) do
+      Ronin::ASM::Program.new(arch: :x86_64) do
+        mov rax, 0xff
+        ret
+      end
+    end
+
+    it "must return 'BITS 64'" do
+      expect(subject.format_prologue(program)).to eq('BITS 64')
+    end
+  end
 end
