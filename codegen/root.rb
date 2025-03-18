e69cfe711bfacebc8d91ae29a4160910f5f72d90
Postmodern <postmodern.mod3@gmail.com>
2025-03-17 22:28:55 -0700

    DRY up the number of `ROOT` constants in `codegen/`.

--- codegen/root.rb
+++ codegen/root.rb
@@ -18,43 +18,7 @@
 # along with ronin-asm.  If not, see <https://www.gnu.org/licenses/>.
 #
 
-require_relative 'root'
-require_relative 'template_file'
-require_relative 'helpers/ruby'
-
 module CodeGen
-  class InstructionsFile < TemplateFile
-
-    include Helpers::Ruby
-
-    #
-    # Gets or sets the target archiecture.
-    #
-    # @param [String, nil] new_arch
-    # @return [String]
-    # @raise [NotImplementedError]
-    #
-    def self.output_file(new_path=nil)
-      if new_path
-        @output_file = File.join(ROOT,new_path)
-        return new_path
-      else
-        @output_file || raise(NotImplementedError,"output_file was not set in #{self}")
-      end
-    end
-
-    #
-    # Initializes the instructions file.
-    #
-    # @param [Array<#name>] instructions
-    # @param [ERB] template
-    # @param [String] path
-    #
-    def initialize(instructions)
-      @instructions = instructions
-
-      super(self.class.output_file)
-    end
-
-  end
+  # The root directory of the project.
+  ROOT = File.expand_path(File.join(__dir__,'..'))
 end
