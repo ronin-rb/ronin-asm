8b91be413b13e746c63eec79b5a630c8b14d3741
Postmodern <postmodern.mod3@gmail.com>
2025-03-18 21:52:51 -0700

    Added the `X86_64::Operand#sibmem?` method (issue #222).

--- spec/x86_64/operand_spec.rb
+++ spec/x86_64/operand_spec.rb
@@ -15,4 +15,10 @@ describe Ronin::ASM::X86_64::Operand do
   it "must also include Ronin::ASM::X86::Operand" do
     expect(subject.class).to include(Ronin::ASM::X86::Operand)
   end
+
+  describe "#sibmem?" do
+    it "must return false by default" do
+      expect(subject.sibmem?).to be(false)
+    end
+  end
 end
