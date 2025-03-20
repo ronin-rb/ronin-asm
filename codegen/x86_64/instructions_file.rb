52eacc72b4cd1d57a822f74d93caf933735ee274
Postmodern <postmodern.mod3@gmail.com>
2025-03-20 00:56:47 -0700

    Added code generation for instruction method examples (issue #210).

--- codegen/x86_64/instructions_file.rb
+++ codegen/x86_64/instructions_file.rb
@@ -30,6 +30,82 @@ module CodeGen
       template File.join(__dir__,'instructions.rb.erb')
       output_file 'lib/ronin/asm/x86_64/instructions.rb'
 
+      # Example letter registers grouped by register type.
+      EXAMPLE_R64_REGISTERS = %w[rax rbx rcx rdx]
+
+      #
+      # Returns an example immediate operand.
+      #
+      # @param [Symbol] operand_type
+      # @param [Integer] index
+      # @return [String]
+      #
+      def example_immediate(operand_type,index)
+        case operand_type
+        when :imm64 then format('0x%.16x',index+1)
+        else             super(operand_type,index)
+        end
+      end
+
+      #
+      # Returns an example register.
+      #
+      # @param [Symbol] operand_type
+      # @param [Integer] index
+      # @return [String]
+      #
+      def example_register(operand_type,index)
+        case operand_type
+        when :r64 then EXAMPLE_R64_REGISTERS.fetch(index)
+        when :tmm then "tmm#{index}"
+        when :rax then 'rax'
+        else           super(operand_type,index)
+        end
+      end
+
+      #
+      # Returns an example memory register for the base of a memory operand.
+      #
+      # @param [Integer] index
+      # @return [String]
+      #
+      def example_memory_base_register(index) = example_register(:r64,index)
+
+      #
+      # Generates an example `rel32m` operand.
+      #
+      # @param [Integer] index
+      # @return [String]
+      #
+      def example_rel32m(index) = example_memory(:m8,index)
+
+      #
+      # Generates an example `sibmem` operand (a memory operand with scale,
+      # index, and base set).
+      #
+      # @param [Integer] index
+      # @return [String]
+      #
+      def example_sibmem(index)
+        memory_operand(
+          base:  example_memory_base_register(index),
+          index: 'r10'
+        )
+      end
+
+      #
+      # Returns an example ronin-asm operand for the x86-64 ISA opperand.
+      #
+      # @param [ISA::Operand] operand
+      # @return [String]
+      #
+      def example_operand(operand,index)
+        if    operand.rel32m? then example_rel32m(index)
+        elsif operand.sibmem? then example_sibmem(index)
+        else                       super(operand,index)
+        end
+      end
+
     end
   end
 end
