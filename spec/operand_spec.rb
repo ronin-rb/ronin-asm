dfbb2b102eda3b3759b34d88c5fc6af4155cddd1
Postmodern <postmodern.mod3@gmail.com>
2025-03-21 23:21:47 -0700

    Added `Operand#type_of?` (closes #233).

--- spec/operand_spec.rb
+++ spec/operand_spec.rb
@@ -6,6 +6,12 @@ describe Ronin::ASM::Operand do
     class Operand
       include Ronin::ASM::Operand
     end
+
+    class OperandWithType
+      include Ronin::ASM::Operand
+
+      def type = :operand32
+    end
   end
 
   let(:test_class) { TestOperand::Operand }
@@ -107,6 +113,24 @@ describe Ronin::ASM::Operand do
   describe "#mem64?" do
     it "must return false by default" do
       expect(subject.mem64?).to be(false)
+    end
+  end
+
+  describe "#type_of?" do
+    context "when the given type equals #type" do
+      let(:test_class) { TestOperand::OperandWithType }
+
+      it "must return true" do
+        expect(subject.type_of?(subject.type)).to be(true)
+      end
+    end
+
+    context "when the given type does not equal #type" do
+      let(:test_class) { TestOperand::OperandWithType }
+
+      it "must return false" do
+        expect(subject.type_of?(:foo)).to be(false)
+      end
     end
   end
 
