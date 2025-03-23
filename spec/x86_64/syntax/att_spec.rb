85ff1ccbd746d3c7c861122dc46dd483a7343862
Postmodern <postmodern.mod3@gmail.com>
2025-03-23 15:25:31 -0700

    Add missing `require`s to `spec/x86_64/syntax/att_spec.rb`.

--- spec/x86_64/syntax/att_spec.rb
+++ spec/x86_64/syntax/att_spec.rb
@@ -1,6 +1,12 @@
 require 'spec_helper'
 require 'ronin/asm/x86_64/syntax/att'
 
+require 'ronin/asm/x86_64/immediate'
+require 'ronin/asm/x86_64/registers'
+require 'ronin/asm/x86_64/memory'
+require 'ronin/asm/x86_64/instruction'
+require 'ronin/asm/program'
+
 describe Ronin::ASM::X86_64::Syntax::ATT do
   it "must inherit from Ronin::ASM::X86::Syntax::ATT" do
     expect(described_class).to be < Ronin::ASM::X86::Syntax::ATT
