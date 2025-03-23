a0a51ce4ab2159249e595e94dbb85c1a0022f672
Postmodern <postmodern.mod3@gmail.com>
2025-03-23 14:18:44 -0700

    Intel syntax does sometimes require an operand size specifier (closes #241).
    * If both operands have an ambigious size, then a size specifier must be
      added to at least one of the operands.
    
      ### Invalid
    
      ```asm
      mov [eax], 0xff
      ```
    
      ### Valid
    
      ```asm
      mov BYTE [eax], 0xff
      mov [eax], BYTE 0xff
      ```

--- spec/x86_64/syntax/intel_spec.rb
+++ spec/x86_64/syntax/intel_spec.rb
@@ -8,6 +8,25 @@ describe Ronin::ASM::X86_64::Syntax::Intel do
 
   subject { described_class }
 
+  describe ".format_operands" do
+    context "when the first operand is of type :mem and the second operand is of type :imm" do
+      let(:register)  { Ronin::ASM::X86_64::Registers::RBX }
+      let(:memory)    { Ronin::ASM::X86_64::Memory.new(base: register) }
+      let(:immediate) { Ronin::ASM::X86_64::Immediate.new(value) }
+      let(:operands)  { [memory, immediate] }
+
+      context "and the immediate's value bit length is between 64 and 32 bits" do
+        let(:value) { 0xffffffffffffffff }
+
+        it "must add a QWORD size specifier to the immediate operand to disambiguate their desired sizes" do
+          expect(subject.format_operands(operands)).to eq(
+            "QWORD #{subject.format_memory(memory)},\t#{subject.format_immediate(immediate)}"
+          )
+        end
+      end
+    end
+  end
+
   describe ".format_prologue" do
     let(:program) do
       Ronin::ASM::Program.new(arch: :x86_64) do
