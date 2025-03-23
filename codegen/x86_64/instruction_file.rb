feba22a3154f6003e5a53eb9e05eef258908681d
Postmodern <postmodern.mod3@gmail.com>
2025-03-23 16:12:05 -0700

    Moved `.erb` files into `codegen/*/templates/` to avoid confusion.

--- codegen/x86_64/instruction_file.rb
+++ codegen/x86_64/instruction_file.rb
@@ -27,7 +27,7 @@ module CodeGen
     #
     class InstructionFile < X86::InstructionFile
 
-      template File.join(__dir__,'instruction.rb.erb')
+      template File.join(__dir__,'templates','instruction.rb.erb')
       output_dir 'lib/ronin/asm/x86_64/instructions'
 
       #
