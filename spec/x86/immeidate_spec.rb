2242a7141f5f95b5c00088bd42abba1a795cf758
Postmodern <postmodern.mod3@gmail.com>
2025-03-23 14:54:16 -0700

    Added `X86::Immediate#infer_size` and `X86_64::Immediate#infer_size`.

--- spec/x86/immeidate_spec.rb
+++ spec/x86/immeidate_spec.rb
@@ -14,6 +14,42 @@ describe Ronin::ASM::X86::Immediate do
 
   subject { described_class.new(value) }
 
+  describe "#infer_size" do
+    context "when the immediate's value fits within 8 bits" do
+      let(:value) { 0xff }
+
+      it "must return 1" do
+        expect(subject.infer_size).to be(1)
+      end
+    end
+
+    context "when the immediate's value fits within 16 bits" do
+      let(:value) { 0xffff }
+
+      it "must return 2" do
+        expect(subject.infer_size).to be(2)
+      end
+    end
+
+    context "when the immediate's value fits within 32 bits" do
+      let(:value) { 0xffffffff }
+
+      it "must return 4" do
+        expect(subject.infer_size).to be(4)
+      end
+    end
+
+    context "when the immediate's value is greater than 32 bits" do
+      let(:value) { 1 << 32 }
+
+      it do
+        expect {
+          subject.infer_size
+        }.to raise_error(TypeError,"immediate operand has a value larger than 32 bits: #{subject.inspect}")
+      end
+    end
+  end
+
   describe "#to_s" do
     it "must call Ronin::ASM::X86::Syntax::Intel.format_immediate" do
       expect(subject.to_s).to eq(
