a73fed788520d4dab495f01dfe775ab59784e7a7
Postmodern <postmodern.mod3@gmail.com>
2025-03-23 15:08:09 -0700

    ATT syntax does sometimes require a size suffix (closes #245).
    * If both operands have an ambiguous size, then an instruction size
      suffix is required to disambiguate the desired operand size.
    
      ### Invalid
    
      ```asm
      mov	$0xff,(%eax)
      ```
      ```
      Warning: no instruction mnemonic suffix given and no register operands; using default for `mov'
      ```
    
      ### Valid
    
      ```asm
      movb	$0xff,(%eax)
      ```
    * Added `X86::Syntax::ATT.format_instruction_name` to contain additional
      instruction size suffix logic.

--- spec/x86_64/syntax/att_spec.rb
+++ spec/x86_64/syntax/att_spec.rb
@@ -8,6 +8,26 @@ describe Ronin::ASM::X86_64::Syntax::ATT do
 
   subject { described_class }
 
+  describe ".format_instruction_name" do
+    context "when the instruction's first operand is of type :mem and the second operand is of type :imm" do
+      let(:register)    { Ronin::ASM::X86_64::Registers::RAX }
+      let(:memory)      { Ronin::ASM::X86_64::Memory.new(base: register) }
+      let(:immediate)   { Ronin::ASM::X86_64::Immediate.new(value) }
+      let(:operands)    { [memory, immediate] }
+      let(:instruction) { Ronin::ASM::X86_64::Instruction.new(:mov, *operands) }
+
+      context "and the immediate's value fits within 64 bits" do
+        let(:value) { 0xffffffffffffffff }
+
+        it "must return the instruction name plus a 'q' size suffix to disambiguate desired instruction operand sizes" do
+          expect(subject.format_instruction_name(instruction)).to eq(
+            "#{instruction.name}q"
+          )
+        end
+      end
+    end
+  end
+
   describe ".format_prologue" do
     let(:program) do
       Ronin::ASM::Program.new(arch: :x86_64) do
