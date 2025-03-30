ddb0bac64f5641c1cd0bc01b73d5bd43d7852d69
Postmodern <postmodern.mod3@gmail.com>
2025-03-30 00:18:02 -0700

    Added `{rn-sae}`, `{rd-sae}`, `{ru-sae}`, and `{rz-sae}` operands (closes #258).
    * Replaced `X86::SpecialOperand` with the `X86::ER` and `X86::SAE`
      classes.
    * Replaced `X86_64::SpecialOperand` with the `X86_64::ER` and `X86_64::SAE`
      classes.
    * Added `X86::Operand#er?` and `#sae?` predicate methods.
    * Added `RN_SAE`, `RD_SAE`, `RU_SAE`, and
      `RZ_SAE` constants to both `X86::Operands` and `X86_64::Operands`.
    * Added the `rn_sae`, `rd_sae`, `ru_sae`, and `rz_sae` methods to both
      `X86` and `X86_64` modules.
    * Updated `X86::Encoder#write_evex` and `X86_64::Encoder#write_evex` to
      correctly encode `{rn-sae}` as `00`, `{rd-sae}` as `01`, `{ru-sae}` as
      `10`, and `{rz-sae}` as `11` into EVEX bits L'L.
      https://en.wikipedia.org/wiki/EVEX_prefix#Technical_description

--- spec/x86/operands_spec.rb
+++ spec/x86/operands_spec.rb
@@ -2,19 +2,49 @@ require 'spec_helper'
 require 'ronin/asm/x86/operands'
 
 describe Ronin::ASM::X86::Operands do
-  describe "ER" do
-    subject { described_class::ER }
+  describe "RN_SAE" do
+    subject { described_class::RN_SAE }
 
-    it "must have a #type of :'{er}'" do
-      expect(subject.type).to eq(:"{er}")
+    it { expect(subject).to be_a(Ronin::ASM::X86::ER) }
+
+    it "must have a #name of :'{rn-sae}'" do
+      expect(subject.name).to eq(:"{rn-sae}")
+    end
+  end
+
+  describe "RD_SAE" do
+    subject { described_class::RD_SAE }
+
+    it { expect(subject).to be_a(Ronin::ASM::X86::ER) }
+
+    it "must have a #name of :'{rd-sae}'" do
+      expect(subject.name).to eq(:"{rd-sae}")
+    end
+  end
+
+  describe "RU_SAE" do
+    subject { described_class::RU_SAE }
+
+    it { expect(subject).to be_a(Ronin::ASM::X86::ER) }
+
+    it "must have a #name of :'{ru-sae}'" do
+      expect(subject.name).to eq(:"{ru-sae}")
+    end
+  end
+
+  describe "RZ_SAE" do
+    subject { described_class::RZ_SAE }
+
+    it { expect(subject).to be_a(Ronin::ASM::X86::ER) }
+
+    it "must have a #name of :'{rz-sae}'" do
+      expect(subject.name).to eq(:"{rz-sae}")
     end
   end
 
   describe "SAE" do
     subject { described_class::SAE }
 
-    it "must have a #type of :'{sae}'" do
-      expect(subject.type).to eq(:"{sae}")
-    end
+    it { expect(subject).to be_a(Ronin::ASM::X86::SAE) }
   end
 end
