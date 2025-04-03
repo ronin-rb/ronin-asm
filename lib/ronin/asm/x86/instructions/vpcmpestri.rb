8cc847ff7b5062cabe1a213017fbc54bc285506e
Postmodern <postmodern.mod3@gmail.com>
2025-04-02 20:15:54 -0700

    Corrected `X86::Instructions::VPCMPESTRI#gas_name`.
    * GNU Assembler (GAS) does not accept `vpcmpestri` with a `l` suffix,
      but does accept `vpcmpestri` without a suffix.

--- lib/ronin/asm/x86/instructions/vpcmpestri.rb
+++ lib/ronin/asm/x86/instructions/vpcmpestri.rb
@@ -61,23 +61,6 @@ module Ronin
           end
 
           #
-          # Returns the GNU Assembler (GAS) name for the instruction based on
-          # it's operands.
-          #
-          # @return [Symbol]
-          #
-          def gas_name
-            case @form
-            when [:xmm, :xmm, :imm8]
-              :vpcmpestril
-            when [:xmm, :mem128, :imm8]
-              :vpcmpestril
-            else
-              super
-            end
-          end
-
-          #
           # Encodes the `vpcmpestri` instruction.
           #
           # @param [Encoder] encoder
