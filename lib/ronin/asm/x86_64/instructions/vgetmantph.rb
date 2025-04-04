1fe6da476d6e526ad0e962abed05f2929a7b74e7
Postmodern <postmodern.mod3@gmail.com>
2025-04-04 14:00:45 -0700

    Updated the x86-64 instructions with corrected encoding logic (#272).

--- lib/ronin/asm/x86_64/instructions/vgetmantph.rb
+++ lib/ronin/asm/x86_64/instructions/vgetmantph.rb
@@ -120,7 +120,7 @@ module Ronin
               encoder.write_modrm(0b11,@operands[0],@operands[1]) +
               encoder.write_immediate(@operands[2],1)
             when [:"zmm{k}{z}", :zmm, :imm8]
-              encoder.write_evex(mmm: 0b011, pp: 0b00, w: 0, ll: 0b10, vvvv: 0, v: 0, rr: @operands[0], _B: @operands[1], x: @operands[1], b: 0, aaa: @operands[0], z: @operands[0]) +
+              encoder.write_evex(mmm: 0b011, pp: 0b00, ll: 0b10, w: 0, vvvv: 0, v: 0, rr: @operands[0], _B: @operands[1], x: @operands[1], b: 0, aaa: @operands[0], z: @operands[0]) +
               encoder.write_opcode(0x26) +
               encoder.write_modrm(0b11,@operands[0],@operands[1]) +
               encoder.write_immediate(@operands[2],1)
@@ -150,7 +150,7 @@ module Ronin
               encoder.write_modrm(@operands[1],@operands[0],@operands[1]) +
               encoder.write_immediate(@operands[2],1)
             when [:zmm, :zmm, :imm8]
-              encoder.write_evex(mmm: 0b011, pp: 0b00, w: 0, ll: 0b10, vvvv: 0, v: 0, rr: @operands[0], _B: @operands[1], x: @operands[1], b: 0, aaa: 0, z: 0) +
+              encoder.write_evex(mmm: 0b011, pp: 0b00, ll: 0b10, w: 0, vvvv: 0, v: 0, rr: @operands[0], _B: @operands[1], x: @operands[1], b: 0, aaa: 0, z: 0) +
               encoder.write_opcode(0x26) +
               encoder.write_modrm(0b11,@operands[0],@operands[1]) +
               encoder.write_immediate(@operands[2],1)
