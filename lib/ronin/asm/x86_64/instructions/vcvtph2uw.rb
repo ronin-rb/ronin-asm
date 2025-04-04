edd32ac33c6dabad115512f5fafea4010a13e0d9
Postmodern <postmodern.mod3@gmail.com>
2025-04-04 11:23:20 -0700

    Specify `ll:` when calling `write_evex` if operands contain `zmm`/`ymm`.
    * This more closely matches the GNU Assembler's (GAS) encoding logic.
    * The x86/x86-64 ISA XML incorrectly omits the `EVEX.LL` value for
      vector instructions that use the `zmm` or `ymm` registers.

--- lib/ronin/asm/x86_64/instructions/vcvtph2uw.rb
+++ lib/ronin/asm/x86_64/instructions/vcvtph2uw.rb
@@ -115,7 +115,7 @@ module Ronin
               encoder.write_opcode(0x7d) +
               encoder.write_modrm(0b11,@operands[0],@operands[1])
             when [:"zmm{k}{z}", :zmm]
-              encoder.write_evex(mmm: 0b101, pp: 0b00, w: 0, vvvv: 0, v: 0, rr: @operands[0], _B: @operands[1], x: @operands[1], b: 0, aaa: @operands[0], z: @operands[0]) +
+              encoder.write_evex(mmm: 0b101, pp: 0b00, w: 0, ll: 0b10, vvvv: 0, v: 0, rr: @operands[0], _B: @operands[1], x: @operands[1], b: 0, aaa: @operands[0], z: @operands[0]) +
               encoder.write_opcode(0x7d) +
               encoder.write_modrm(0b11,@operands[0],@operands[1])
             when [:xmm, :"mem128/mem16bcst"]
@@ -139,7 +139,7 @@ module Ronin
               encoder.write_opcode(0x7d) +
               encoder.write_modrm(@operands[1],@operands[0],@operands[1])
             when [:zmm, :zmm]
-              encoder.write_evex(mmm: 0b101, pp: 0b00, w: 0, vvvv: 0, v: 0, rr: @operands[0], _B: @operands[1], x: @operands[1], b: 0, aaa: 0, z: 0) +
+              encoder.write_evex(mmm: 0b101, pp: 0b00, w: 0, ll: 0b10, vvvv: 0, v: 0, rr: @operands[0], _B: @operands[1], x: @operands[1], b: 0, aaa: 0, z: 0) +
               encoder.write_opcode(0x7d) +
               encoder.write_modrm(0b11,@operands[0],@operands[1])
             when [:"zmm{k}{z}", :zmm, :"{er}"]
