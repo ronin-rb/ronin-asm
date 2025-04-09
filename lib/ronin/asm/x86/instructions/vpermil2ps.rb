27793103abdc6de53d2fb10b403d0d69ada15f4b
Postmodern <postmodern.mod3@gmail.com>
2025-04-08 22:36:35 -0700

    Changed the encoding of x86 instruction `vpermil2ps xmm, xmm, xmm, xmm, imm8`.
    * Changed the encoding logic for instruction form
      `vpermil2ps xmm, xmm, xmm, xmm, imm8` to match the encoding logic of
      GNU Assembler (GAS).
    
      GAS:
    
      ```asm
      vpermil2ps  $0x4, %xmm3, %xmm2, %xmm1, %xmm0
      ```
      ```
      c4 e3 71 48 c2 34
      ```

--- lib/ronin/asm/x86/instructions/vpermil2ps.rb
+++ lib/ronin/asm/x86/instructions/vpermil2ps.rb
@@ -79,10 +79,10 @@ module Ronin
           def encode(encoder)
             case @form
             when [:xmm, :xmm, :xmm, :xmm, :imm8]
-              encoder.write_vex(type: :vex, w: 1, l: 0, m_mmmm: 0b00011, pp: 0b01, r: 0, x: 0, b: 0, vvvv: @operands[1]) +
+              encoder.write_vex(type: :vex, w: 0, l: 0, m_mmmm: 0b00011, pp: 0b01, r: 0, x: 0, b: 0, vvvv: @operands[1]) +
               encoder.write_opcode(0x48) +
-              encoder.write_modrm(0b11,@operands[0],@operands[3]) +
-              encoder.write_register_byte(@operands[2],@operands[4])
+              encoder.write_modrm(0b11,@operands[0],@operands[2]) +
+              encoder.write_register_byte(@operands[3],@operands[4])
             when [:xmm, :xmm, :xmm, :mem128, :imm8]
               encoder.write_vex(type: :vex, w: 1, l: 0, m_mmmm: 0b00011, pp: 0b01, r: 0, x: 0, b: 0, vvvv: @operands[1]) +
               encoder.write_opcode(0x48) +
