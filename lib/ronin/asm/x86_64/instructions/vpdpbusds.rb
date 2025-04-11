d5bb50356fb7c189c9c683ba293573d4446fe7a8
Postmodern <postmodern.mod3@gmail.com>
2025-04-10 21:01:45 -0700

    Changed the encoding for x86-64 instruction `vpdpbusds xmm, xmm, xmm`.
    * Use EVEX encoding instead of VEX encoding to match the encoding logic of
      both GNU Assembler (GAS) and NASM.
    
      GAS:
    
      ```asm
      vpdpbusds  %xmm2, %xmm1, %xmm0
      ```
      ```
      62 f2 75 08 51 c2
      ```
    
      NASM:
    
      ```asm
      vpdpbusds  xmm0, xmm1, xmm2
      ```
      ```
      62 f2 75 08 51 c2
      ```
    * AVX512-VNNI changed `vpdpbusds` to use EVEX encoding.
      https://en.wikipedia.org/wiki/AVX-512#VNNI

--- lib/ronin/asm/x86_64/instructions/vpdpbusds.rb
+++ lib/ronin/asm/x86_64/instructions/vpdpbusds.rb
@@ -123,7 +123,7 @@ module Ronin
               encoder.write_opcode(0x51) +
               encoder.write_modrm(@operands[2],@operands[0],@operands[2])
             when [:xmm, :xmm, :xmm]
-              encoder.write_vex(type: :vex, w: 0, l: 0, m_mmmm: 0b00010, pp: 0b01, r: @operands[0], b: @operands[2], vvvv: @operands[1]) +
+              encoder.write_evex(mmm: 0b010, pp: 0b01, ll: 0b00, w: 0, vvvv: @operands[1], v: @operands[1], rr: @operands[0], _B: @operands[2], x: @operands[2], b: 0, aaa: 0, z: 0) +
               encoder.write_opcode(0x51) +
               encoder.write_modrm(0b11,@operands[0],@operands[2])
             when [:xmm, :xmm, :mem128]
