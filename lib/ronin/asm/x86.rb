406fbef74f730a1fe4f4f6cef9d9f5d1cc725bf3
Postmodern <postmodern.mod3@gmail.com>
2025-05-01 17:30:29 -0700

    Added missing `@return` tags to `X86` and `X86_64` methods.

--- lib/ronin/asm/x86.rb
+++ lib/ronin/asm/x86.rb
@@ -53,225 +53,373 @@ module Ronin
       #
 
       # The `al` 8bit "accumulator" register.
+      #
+      # @return [Register]
       def al = Registers::AL
 
       # The `ah` 8bit "accumulator" register.
+      #
+      # @return [Register]
       def ah = Registers::AH
 
       # The `ax` 16bit "accumulator" register.
+      #
+      # @return [Register]
       def ax = Registers::AX
 
       # The `eax` 32bit "accumulator" register.
+      #
+      # @return [Register]
       def eax = Registers::EAX
 
       # The `bl` 8bit "base" register.
+      #
+      # @return [Register]
       def bl = Registers::BL
 
       # The `bh` 8bit "base" register.
+      #
+      # @return [Register]
       def bh = Registers::BH
 
       # The `bx` 16bit "base" register.
+      #
+      # @return [Register]
       def bx = Registers::BX
 
       # The `ebx` 32bit "base" register.
+      #
+      # @return [Register]
       def ebx = Registers::EBX
 
       # The `cl` 8bit "counter" register.
+      #
+      # @return [Register]
       def cl = Registers::CL
 
       # The `ch` 8bit "counter" register.
+      #
+      # @return [Register]
       def ch = Registers::CH
 
       # The `cx` 16bit "counter" register.
+      #
+      # @return [Register]
       def cx = Registers::CX
 
       # The `ecx` 32bit "counter" register.
+      #
+      # @return [Register]
       def ecx = Registers::ECX
 
       # The `dl` 8bit "data" register.
+      #
+      # @return [Register]
       def dl = Registers::DL
 
       # The `dh` 8bit "data" register.
+      #
+      # @return [Register]
       def dh = Registers::DH
 
       # The `dx` 16bit "data" register.
+      #
+      # @return [Register]
       def dx = Registers::DX
 
       # The `edx` 32bit "data" register.
+      #
+      # @return [Register]
       def edx = Registers::EDX
 
       # The `sil` 8bit "source" register.
+      #
+      # @return [Register]
       def sil = Registers::SIL
 
       # The `si` 16bit "source" register.
+      #
+      # @return [Register]
       def si = Registers::SI
 
       # The `esi` 32bit "source" register.
+      #
+      # @return [Register]
       def esi = Registers::ESI
 
       # The `dil` 8bit "destination" register.
+      #
+      # @return [Register]
       def dil = Registers::DIL
 
       # The `di` 16bit "destination" register.
+      #
+      # @return [Register]
       def di = Registers::DI
 
       # The `edi` 32bit "destination" register.
+      #
+      # @return [Register]
       def edi = Registers::EDI
 
       # The `bpl` 8bit stack base pointer register.
+      #
+      # @return [Register]
       def bpl = Registers::BPL
 
       # The `bp` 16bit stack base pointer register.
+      #
+      # @return [Register]
       def bp = Registers::BP
 
       # The `ebp` 32bit stack base pointer register.
+      #
+      # @return [Register]
       def ebp = Registers::EBP
 
       # The `spl` 8bit stack pointer register.
+      #
+      # @return [Register]
       def spl = Registers::SPL
 
       # The `sp` 16bit stack pointer register.
+      #
+      # @return [Register]
       def sp = Registers::SP
 
       # The `esp` 32bit stack pointer register.
+      #
+      # @return [Register]
       def esp = Registers::ESP
 
       # The `cs` 16bit Code Segment register.
+      #
+      # @return [Register]
       def cs = Registers::CS
 
       # The `ds` 16bit Data Segment register.
+      #
+      # @return [Register]
       def ds = Registers::DS
 
       # The `es` 16bit Extra Segment register.
+      #
+      # @return [Register]
       def es = Registers::ES
 
       # The `fs` 16bit F Segment register.
+      #
+      # @return [Register]
       def fs = Registers::FS
 
       # The `gs` 16bit G Segment register.
+      #
+      # @return [Register]
       def gs = Registers::GS
 
       # The `ss` 16bit Stack Segment register.
+      #
+      # @return [Register]
       def ss = Registers::SS
 
       # The `k0` 64bit opmask register.
+      #
+      # @return [Register]
       def k0 = Registers::K0
 
       # The `k1` 64bit opmask register.
+      #
+      # @return [Register]
       def k1 = Registers::K1
 
       # The `k2` 64bit opmask register.
+      #
+      # @return [Register]
       def k2 = Registers::K2
 
       # The `k3` 64bit opmask register.
+      #
+      # @return [Register]
       def k3 = Registers::K3
 
       # The `k4` 64bit opmask register.
+      #
+      # @return [Register]
       def k4 = Registers::K4
 
       # The `k5` 64bit opmask register.
+      #
+      # @return [Register]
       def k5 = Registers::K5
 
       # The `k6` 64bit opmask register.
+      #
+      # @return [Register]
       def k6 = Registers::K6
 
       # The `k7` 64bit opmask register.
+      #
+      # @return [Register]
       def k7 = Registers::K7
 
       # The `mm0` 64bit MMX register.
+      #
+      # @return [Register]
       def mm0 = Registers::MM0
 
       # The `mm1` 64bit MMX register.
+      #
+      # @return [Register]
       def mm1 = Registers::MM1
 
       # The `mm2` 64bit MMX register.
+      #
+      # @return [Register]
       def mm2 = Registers::MM2
 
       # The `mm3` 64bit MMX register.
+      #
+      # @return [Register]
       def mm3 = Registers::MM3
 
       # The `mm4` 64bit MMX register.
+      #
+      # @return [Register]
       def mm4 = Registers::MM4
 
       # The `mm5` 64bit MMX register.
+      #
+      # @return [Register]
       def mm5 = Registers::MM5
 
       # The `mm6` 64bit MMX register.
+      #
+      # @return [Register]
       def mm6 = Registers::MM6
 
       # The `mm7` 64bit MMX register.
+      #
+      # @return [Register]
       def mm7 = Registers::MM7
 
       # The `xmm0` 128bit XMM register.
+      #
+      # @return [Register]
       def xmm0 = Registers::XMM0
 
       # The `xmm1` 128bit XMM register.
+      #
+      # @return [Register]
       def xmm1 = Registers::XMM1
 
       # The `xmm2` 128bit XMM register.
+      #
+      # @return [Register]
       def xmm2 = Registers::XMM2
 
       # The `xmm3` 128bit XMM register.
+      #
+      # @return [Register]
       def xmm3 = Registers::XMM3
 
       # The `xmm4` 128bit XMM register.
+      #
+      # @return [Register]
       def xmm4 = Registers::XMM4
 
       # The `xmm5` 128bit XMM register.
+      #
+      # @return [Register]
       def xmm5 = Registers::XMM5
 
       # The `xmm6` 128bit XMM register.
+      #
+      # @return [Register]
       def xmm6 = Registers::XMM6
 
       # The `xmm7` 128bit XMM register.
+      #
+      # @return [Register]
       def xmm7 = Registers::XMM7
 
       # The `ymm0` 256bit YMM register.
+      #
+      # @return [Register]
       def ymm0 = Registers::YMM0
 
       # The `ymm1` 256bit YMM register.
+      #
+      # @return [Register]
       def ymm1 = Registers::YMM1
 
       # The `ymm2` 256bit YMM register.
+      #
+      # @return [Register]
       def ymm2 = Registers::YMM2
 
       # The `ymm3` 256bit YMM register.
+      #
+      # @return [Register]
       def ymm3 = Registers::YMM3
 
       # The `ymm4` 256bit YMM register.
+      #
+      # @return [Register]
       def ymm4 = Registers::YMM4
 
       # The `ymm5` 256bit YMM register.
+      #
+      # @return [Register]
       def ymm5 = Registers::YMM5
 
       # The `ymm6` 256bit YMM register.
+      #
+      # @return [Register]
       def ymm6 = Registers::YMM6
 
       # The `ymm7` 256bit YMM register.
+      #
+      # @return [Register]
       def ymm7 = Registers::YMM7
 
       # The `zmm0` 512bit ZMM register.
+      #
+      # @return [Register]
       def zmm0 = Registers::ZMM0
 
       # The `zmm1` 512bit ZMM register.
+      #
+      # @return [Register]
       def zmm1 = Registers::ZMM1
 
       # The `zmm2` 512bit ZMM register.
+      #
+      # @return [Register]
       def zmm2 = Registers::ZMM2
 
       # The `zmm3` 512bit ZMM register.
+      #
+      # @return [Register]
       def zmm3 = Registers::ZMM3
 
       # The `zmm4` 512bit ZMM register.
+      #
+      # @return [Register]
       def zmm4 = Registers::ZMM4
 
       # The `zmm5` 512bit ZMM register.
+      #
+      # @return [Register]
       def zmm5 = Registers::ZMM5
 
       # The `zmm6` 512bit ZMM register.
+      #
+      # @return [Register]
       def zmm6 = Registers::ZMM6
 
       # The `zmm7` 512bit ZMM register.
+      #
+      # @return [Register]
       def zmm7 = Registers::ZMM7
 
       #
@@ -280,6 +428,8 @@ module Ronin
 
       #
       # The `{rn-sae}` decorator operand.
+      #
+      # @return [ER]
       #
       # @example
       #   vaddpd zmm1, zmm2, zmm3, rn_sae
@@ -291,6 +441,8 @@ module Ronin
       #
       # The `{rd-sae}` decorator operand.
       #
+      # @return [ER]
+      #
       # @example
       #   vaddpd zmm1, zmm2, zmm3, rd_sae
       #
@@ -300,6 +452,8 @@ module Ronin
 
       #
       # The `{ru-sae}` decorator operand.
+      #
+      # @return [ER]
       #
       # @example
       #   vaddpd zmm1, zmm2, zmm3, ru_sae
@@ -311,6 +465,8 @@ module Ronin
       #
       # The `{rz-sae}` decorator operand.
       #
+      # @return [ER]
+      #
       # @example
       #   vaddpd zmm1, zmm2, zmm3, rz_sae
       #
@@ -320,6 +476,8 @@ module Ronin
 
       #
       # The `{sae}` operand.
+      #
+      # @return [SAE]
       #
       # @example
       #   vcomisd xmm1, xmm2, sae
@@ -499,6 +657,8 @@ module Ronin
       #
       # The Stack Base Pointer register.
       #
+      # @return [Register]
+      #
       # @api private
       #
       # @see #ebp
@@ -507,6 +667,8 @@ module Ronin
 
       #
       # The Stack Pointer register.
+      #
+      # @return [Register]
       #
       # @api private
       #
