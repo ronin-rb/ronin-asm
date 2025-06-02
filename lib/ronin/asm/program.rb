4bdabc2bdfcf9f2460f82a860c4c833e4b004eef
Postmodern <postmodern.mod3@gmail.com>
2025-06-02 14:05:42 -0700

    Added top-level documentation to `Ronin::ASM::Program` (closes #206).

--- lib/ronin/asm/program.rb
+++ lib/ronin/asm/program.rb
@@ -32,7 +32,302 @@ require 'stringio'
 module Ronin
   module ASM
     #
-    # Represents Assembly code.
+    # The {Program} class allows writing and representing Assembly programs in
+    # Ruby.
+    # These programs can then be inspected, modified, assembled to a binary
+    # String or even converted back into Assembly source code.
+    #
+    # ## Features
+    #
+    # * Supports x86 and x86-64 architectures.
+    # * Provides a Ruby DSL for representing Assembly that is nearly identical
+    #   to Intel Assembly syntax.
+    # * Supports nearly every feature of the Assembly programming language:
+    #   * Labels
+    #   * Symbols
+    #   * Instructions
+    #   * Registers
+    #   * Immediate values
+    #   * Memory operands:
+    #     * Displacements
+    #     * Scale Index Base (SIB) memory addressing
+    #     * RIP-relative addressing
+    #   * Operand size keywords
+    #   * Memory Broadcasts
+    #   * Opmasks (aka Write Masks)
+    #   * Rounding Control
+    #   * Macros
+    # * Supports syscalls for Linux, FreeBSD, NetBSD, OpenBSD, and macOS.
+    # * Supports assembling {Program}s to a binary String or writing directly
+    #   to an output stream.
+    # * Supports converting programs back into Assembly syntax (Intel or ATT).
+    #
+    # ## Examples
+    #
+    #     asm = Ronin::ASM.new do
+    #       xor rdx, rdx                # set rdx (envp) to NULL
+    #       mov rbx, 0x68732f6e69622f2f # set rbx to "//bin/sh"
+    #       shr rbx, 8                  # add a \0 byte by shifting right one byte
+    #       push rbx                    # push "/bin/sh\0" to the stack
+    #       mov rdi, rsp                # rdi (pathname) points onto the stack
+    #       push rdx                    # push NULL onto the stack
+    #       push rdi                    # push pointer to pathname
+    #       mov rsi, rsp                # rsi (argv) points to the stack
+    #       mov al, 0x3b                # Linux x86-64 syscall number for execve()
+    #       syscall                     # call execve(pathname,argv,envp)
+    #     end
+    #
+    #     payload = asm.assemble
+    #     # => "H1\xD2H\xBB//bin/shH\xC1\xEB\bSH\x89\xE7PWH\x89\xE6\xC6\xC0;\x0F\x05"
+    #
+    #     puts asm.to_asm
+    #
+    # ### Architectures
+    #
+    # By default all Assembly programs use the x86-64 architecture:
+    #
+    #     asm = Ronin::ASM::Program.new do
+    #       mov rax, 0x112233445566778899
+    #       # ...
+    #     end
+    #
+    # An explicit architecture can also be specified:
+    #
+    #     asm = Ronin::ASM.new(arch: :x86) do
+    #       mov eax, 0x11223344
+    #       # ...
+    #     end
+    #
+    # The following architectures are supported:
+    #
+    # * `:x86` - x86, aka i386/i486/i586/i686.
+    # * `:x86_64` - x86-64.
+    # * `:amd64` - alias for `:x86_64`.
+    #
+    # ### Registers
+    #
+    # All user-space registers are available as methods within the Assembly
+    # program:
+    #
+    #     asm = Ronin::ASM::Program.new do
+    #       mov al,  0xff
+    #       mov ax,  0xffff
+    #       mov eax, 0xffffffff
+    #       mov rax, 0xffffffffffffffff
+    #     end
+    #
+    # ### Immediates
+    #
+    # Immediate values are represented as plain-old Ruby Integers:
+    #
+    #     asm = Ronin::ASM::Program.new do
+    #       mov rax, 0x0a   # hexadecimal
+    #       mov rax, 10     # decimal
+    #       mov rax, 012    # octal
+    #       mov rax, 0b1010 # binary
+    #     end
+    #
+    # Immediate values can also have explicit sizes:
+    #
+    #     asm = Ronin::ASM::Program.new do
+    #       mov al,  byte(0xff)
+    #       mov ax,  word(0xffff)
+    #       mov eax, dword(0xffffffff)
+    #       mov rax, qword(0xffffffffffffffff)
+    #     end
+    #
+    # ### Memory
+    #
+    # Memory operands are represented as Ruby Arrays similar to Intel syntax:
+    #
+    #     asm = Ronin::ASM::Program.new do
+    #       mov rax, [rbx]
+    #       mov rax, [rbx+10]
+    #       mov rax, [rbx+rsi*8]
+    #       mov rax, [rbx+10+rsi*8]
+    #     end
+    #
+    # #### Memory Broadcasts
+    #
+    # Memory broadcasts can also be added to memory operands:
+    #
+    #     asm = Ronin::ASM::Program.new do
+    #       vcvtpd2qq ymm0, bcst(qword([rbx]), {1=>4})
+    #     end
+    #
+    # ### Opmasks
+    #
+    # Opmasks (aka Write Masks) can also be added to destination operands:
+    #
+    #     asm = Ronin::ASM::Program.new do
+    #       vaddpd opmask(xmm1, k1, zero: true), xmm2, xmm3
+    #     end
+    #
+    # ### Rounding Control
+    #
+    # Rounding control can be specified for certain instructions as the last
+    # operand:
+    #
+    #     asm = Ronin::ASM::Program.new do
+    #       vaddpd zmm1, zmm2, zmm3, rn_sae
+    #       vaddpd zmm1, zmm2, zmm3, rd_sae
+    #       vaddpd zmm1, zmm2, zmm3, ru_sae
+    #       vaddpd zmm1, zmm2, zmm3, rz_sae
+    #       vcomisd xmm1, xmm2, sae
+    #     end
+    #
+    # ### Instructions
+    #
+    # Instructions are represented as Ruby method calls without the parenthesis,
+    # with the destination operand first, similar to Intel syntax:
+    #
+    #     asm = Ronin::ASM::Program.new do
+    #       mov rax, 0x41
+    #       push rax
+    #       nop
+    #     end
+    #
+    # See {X86::Instructions} and {X86_64::Instructions} for all instruction
+    # methods.
+    #
+    # ### Labels
+    #
+    # Labels are defined by calling the label name with a block and referenced
+    # with a method call without a block:
+    #
+    #     asm = Ronin::ASM::Program.new do
+    #       xor rcx, rcx
+    #
+    #       _loop do
+    #         inc rcx
+    #         cmp rcx, 10
+    #         jle _loop
+    #       end
+    #     end
+    #
+    # Labels can be referenced by jump/call instructions before they
+    # are defined.
+    #
+    #     asm = Ronin::ASM::Program.new do
+    #       jnz _label
+    #
+    #       # ...
+    #
+    #       _label do
+    #         # ...
+    #       end
+    #     end
+    #
+    # ### Macros
+    #
+    # Simply `name=value` macros can be passed in via the `macros:` keyword
+    # argument as a Hash and accessed within the program as methods:
+    #
+    #     asm = Ronin::ASM::Program.new(macros: {port: 1337}) do
+    #       # ...
+    #       mov ax, port
+    #       # ...
+    #     end
+    #
+    # Macro functions can be defined as methods within the Assembly program:
+    #
+    #     asm = Ronin::ASM::Program.new do
+    #       def randdom_port = rand(1024..65535)
+    #
+    #       mov ax, random_port
+    #       mov bx, random_port
+    #     end
+    #
+    # Macro methods can even accept arguments and call other instructions:
+    #
+    #     asm = Ronin::ASM::Program.new do
+    #       def obfus_mov(reg,int)
+    #         mov reg, (int - 42)
+    #         add reg, 42
+    #       end
+    #
+    #       obfus_mov ax, 1337
+    #     end
+    #
+    # ### Syscalls
+    #
+    # Builtin syscall macros are provided for Linux, FreeBSD, NetBSD, OpenBSD,
+    # and macOS. Specify the `os:` keyword argument to enable syscalls for the
+    # specified OS:
+    #
+    #     asm = Ronin::ASM::Program.new(os: :linux) do
+    #       # ...
+    #       exit_syscall(42)            # call exit(42)
+    #     end
+    #
+    # Syscall macros can also be passed registers as their arguments:
+    #
+    #     asm = Ronin::ASM::Program.new(os: :linux) do
+    #       xor rdx, rdx                # set rdx (envp) to NULL
+    #       mov rbx, 0x68732f6e69622f2f # set rbx to "//bin/sh"
+    #       shr rbx, 8                  # add a \0 byte by shifting right
+    #       push rbx                    # push "/bin/sh\0" to the stack
+    #       mov rdi, rsp                # rdi (pathname) points onto the stack
+    #       push rdx                    # push NULL onto the stack
+    #       push rdi                    # push pointer to pathname
+    #       mov rsi, rsp                # rsi (argv) points to the stack
+    #
+    #       execve_syscall(rdi,rsi,rdx) # call execve(pathname,argv,envp)
+    #     end
+    #
+    # ### Assembling
+    #
+    # Assembly programs can be assembled into a String of bytes by calling
+    # {Program#assemble assemble}:
+    #
+    #     asm = Ronin::ASM::Program.new do
+    #       pop rbp
+    #       ret
+    #     end
+    #
+    #     asm.assemble
+    #     # => "]\xC3"
+    #
+    # Assembly programs can also be assembled and directly written to any IO
+    # object:
+    #
+    #     asm = Ronin::ASM::Program.new do
+    #       # ...
+    #     end
+    #
+    #     File.open('output.bin','wb') do |file|
+    #       # ...
+    #       asm.assemble(file)
+    #       # ...
+    #     end
+    #
+    # ### Syntax
+    #
+    # Assembly programs can be formatted back into Assembly syntax:
+    #
+    #     asm = Ronin::ASM::Program.new do
+    #       push rbx
+    #       mov  rax, qword(0xc0ffee)
+    #       pop  rbx
+    #     end
+    #
+    #     puts asm.to_asm
+    #     # BITS 64
+    #     # section .text
+    #     # _start:
+    #     #	push	rbx
+    #     #	mov	rax,	QWORD 0xc0ffee
+    #     #	pop	rbx
+    #
+    # ATT syntax is also supported:
+    #
+    #     puts asm.to_asm(:att)
+    #     # .code64
+    #     # .text
+    #     # _start:
+    #     #	push	%rbx
+    #     #	mov	$0xc0ffee,      %rax
+    #     #	pop	%rbx
     #
     class Program
 
